9c963a5ddc94f1ede6a0eae770785e97baac8ae1
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-02-25 14:16:59 +0000

    MAN-172 remove comments from code

--- cypress.config.ts
+++ cypress.config.ts
@@ -20,8 +20,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       on('task', {
         log(message) {
-          // Then to see the log messages in the terminal
-          //   cy.task("log", "my message");
           console.log(`${message}\n\n`)
           return null
         },
