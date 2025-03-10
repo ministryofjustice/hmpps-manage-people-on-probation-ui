58d8b2d650adf8df6369637d4eed6b18c4185fe9
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-03-10 19:17:14 +0000

    Refactor property imports

---
+++ server/properties/filterOptions.ts
@@ -0,0 +1,5 @@
+export const filterOptions = [
+  { text: 'Without an outcome', value: 'no outcome' },
+  { text: 'Complied', value: 'complied' },
+  { text: 'Not complied', value: 'not complied' },
+]
