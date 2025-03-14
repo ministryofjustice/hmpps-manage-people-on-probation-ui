9aa3ce3d7b8c464b389de840768453b5a1a9fa94
Neil Mills <35219002+neil-mills@users.noreply.github.com>
2025-03-14 17:11:24 +0000

    MAN-361 Update 'My cases' page title to 'Cases' (#415)

--- integration_tests/pages/myCases.ts
+++ integration_tests/pages/myCases.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from './page'
 
 export default class YourCasesPage extends Page {
   constructor() {
-    super('My cases')
+    super('Cases')
   }
 
   getPagination = (): PageElement => cy.get(`[data-qa=pagination]`)
