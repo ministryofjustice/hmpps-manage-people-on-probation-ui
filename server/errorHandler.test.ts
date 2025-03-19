b2e5b4250903262edc0c1f4e5760bdf8e66cdd6d
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-03-19 15:56:09 +0000

    MAN-455 update 404 and 500 error pages

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -1,6 +1,7 @@
 import type { Express } from 'express'
 import request from 'supertest'
 import { appWithAllRoutes } from './routes/testutils/appSetup'
+import { statusErrors } from './properties'
 
 let app: Express
 
@@ -13,14 +14,16 @@ afterEach(() => {
 })
 
 describe('GET 404', () => {
+  const { title, message } = statusErrors[404]
   it('should render content with stack in dev mode', () => {
     return request(app)
       .get('/unknown')
       .expect(404)
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('NotFoundError: Not Found')
-        expect(res.text).not.toContain('Something went wrong. The error has been logged. Please try again')
+        expect(res.text).toContain(title)
+        expect(res.text).toContain(message)
+        expect(res.text).toContain('Error: 404 Not Found')
       })
   })
 
@@ -30,8 +33,9 @@ describe('GET 404', () => {
       .expect(404)
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('Something went wrong. The error has been logged. Please try again')
-        expect(res.text).not.toContain('NotFoundError: Not Found')
+        expect(res.text).toContain(title)
+        expect(res.text).toContain(message)
+        expect(res.text).not.toContain('Error: 404 Not Found')
       })
   })
 })
--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -2,23 +2,25 @@ import type { Request, NextFunction } from 'express'
 import type { HTTPError } from 'superagent'
 import logger from '../logger'
 import type { AppResponse } from './@types'
+import { statusErrors, type StatusErrorCode } from './properties'
 
 export default function createErrorHandler(production: boolean) {
   return (error: HTTPError, req: Request, res: AppResponse, next: NextFunction): void => {
+    const { status } = error
     logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
+    const { title, message } =
+      statusErrors[statusErrors?.[status as StatusErrorCode] ? (status as StatusErrorCode) : 500]
 
-    if (error.status === 401 || error.status === 403) {
+    if (status === 401 || status === 403) {
       logger.info('Logging user out')
       return res.redirect('/sign-out')
     }
-
-    res.locals.message = production
-      ? 'Something went wrong. The error has been logged. Please try again'
-      : error.message
-    res.locals.status = error.status
+    res.locals.title = title
+    res.locals.message = production ? message : `${message}<p>Error: ${error.status} ${error.message}</p>`
+    res.locals.status = status
     res.locals.stack = production ? null : error.stack
 
-    res.status(error.status || 500)
+    res.status(error?.status || 500)
 
     return res.render('pages/error')
   }
