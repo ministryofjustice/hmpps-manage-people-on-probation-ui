c97f0117642e13c6452e1590bcd59bd7513acc71
Neil Mills <35219002+neil-mills@users.noreply.github.com>
2025-05-06 13:01:55 +0100

    MAN-183 sentence page bug fix (#519)

--- server/middleware/validation/appointments.ts
+++ server/middleware/validation/appointments.ts
@@ -34,7 +34,9 @@ const appointments: Route<void> = (req, res, next) => {
       const type = getDataValue(data, ['appointments', crn, id, 'type'])
       const showReveal = ['Home visit', 'Planned office visit'].includes(type)
       const sentences = req.session.data.sentences[crn]
-      const sentence = sentences.find((s: any) => s.order.description === req.body?.appointments?.[crn]?.[id]?.sentence)
+      const sentence = sentences.find(
+        (s: any) => s?.order?.description === req.body?.appointments?.[crn]?.[id]?.sentence,
+      )
       const validateSentenceRequirement = showReveal && sentence?.requirements?.length > 0
       const validateSentenceLicenceCondition = showReveal && sentence?.licenceConditions?.length > 0
       errorMessages = validateWithSpec(
