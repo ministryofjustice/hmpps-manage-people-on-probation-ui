88f0c0daf671e0276908a39ffabc9851734102b5
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-06-16 11:25:23 +0100

    MAN-633/634 type and sentence pages wip

--- server/models/Appointments.ts
+++ server/models/Appointments.ts
@@ -69,7 +69,7 @@ export interface AppointmentRequestBody {
   end: Date
   interval: AppointmentInterval
   numberOfAppointments: number
-  eventId: number
+  eventNumber: number
   uuid: string
   createOverlappingAppointment: true
   requirementId: number
