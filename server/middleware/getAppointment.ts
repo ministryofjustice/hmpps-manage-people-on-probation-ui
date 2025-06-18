e598dfcd749070298f031b63f7e9000bf9f4cd5b
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-06-18 19:45:35 +0100

    MAN-633-634 complete pages with passing int and unit tests

--- server/middleware/getAppointment.ts
+++ server/middleware/getAppointment.ts
@@ -97,6 +97,7 @@ export const getAppointment = (hmppsAuthClient: HmppsAuthClient): Route<Promise<
       }
     }
     res.locals.appointment = appointment
+    // console.dir(appointment, { depth: null })
     return next()
   }
 }
