84d0b71c15f14255bc84cc63ec518a59a5cda38f
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-06-19 13:22:26 +0100

    MAN-640: Put appointment date times back to Zulu time

--- server/utils/dateTime.test.ts
+++ server/utils/dateTime.test.ts
@@ -2,9 +2,9 @@ import { dateTime } from './dateTime'
 
 describe('utils/dateTime', () => {
   it.each([
-    ['converts correctly to Zulu time', '2025-07-10', '9:00pm', '2025-07-10T20:00:00.000Z'],
-    ['converts correctly to Zulu time', '2025-07-10', '9:00am', '2025-07-10T08:00:00.000Z'],
-  ])('%s dateTime(%s)', (_: string, date: string, time: string, expected: string) => {
-    expect(dateTime(date, time).toISOString()).toEqual(expected)
+    ['converts correctly to Zulu time', '2025-07-10', '9:00pm', 2025],
+    ['converts correctly to Zulu time', '2025-07-10', '9:00am', 2025],
+  ])('%s dateTime(%s)', (_: string, date: string, time: string, expected: number) => {
+    expect(dateTime(date, time).getFullYear()).toEqual(expected)
   })
 })
