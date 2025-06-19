f87938f173080a5286adb4998f7b2c504ae53f2a
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-06-19 13:07:48 +0100

    MAN-640: Put appointment date times back to Zulu time

--- server/utils/dateTime.ts
+++ server/utils/dateTime.ts
@@ -9,7 +9,5 @@ export const dateTime = (date: string, time: string): Date => {
     .map(val => parseInt(val, 10))
   const newHour = isPm && hour !== 12 ? hour + 12 : hour
   const [year, month, day] = date.split('-').map(val => parseInt(val, 10))
-  const newDate = new Date(year, month - 1, day, newHour, minute, 0)
-  newDate.setHours(newDate.getHours() + Math.abs(newDate.getTimezoneOffset() / 60))
-  return newDate
+  return new Date(year, month - 1, day, newHour, minute, 0)
 }
