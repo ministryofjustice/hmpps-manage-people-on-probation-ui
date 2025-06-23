dacc5c0a479a0921c2ec58237c905271e9872284
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2025-06-23 13:27:41 +0100

    MAN-635: process query params (#575)

--- server/middleware/getWhoAttends.ts
+++ server/middleware/getWhoAttends.ts
@@ -5,13 +5,14 @@ import { Route } from '../@types'
 export const getWhoAttends = (hmppsAuthClient: HmppsAuthClient): Route<Promise<void>> => {
   return async (req, res, next) => {
     const { username } = res.locals.user
-    const { regionCode, teamCode, crn, id } = req.params
+    const { crn, id } = req.params
+    const regionCode = req.query.regionCode as string
+    const teamCode = req.query.teamCode as string
     const token = await hmppsAuthClient.getSystemClientToken(username)
     const masClient = new MasApiClient(token)
 
     const region = regionCode || req?.session?.data?.appointments?.[crn]?.[id]?.region
-    const team = teamCode || req?.session?.data?.appointments?.[crn]?.[id]?.team
-    const userProviders = await masClient.getUserProviders(username, region, team)
+    const userProviders = await masClient.getUserProviders(username, region, teamCode)
 
     req.session.data = {
       ...(req?.session?.data ?? {}),
