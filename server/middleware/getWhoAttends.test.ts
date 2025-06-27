203e582470317e0577bdd99867c51bdbf4757f0f
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-06-27 20:38:08 +0100

    MAN-689: set default drop down values

--- server/middleware/getWhoAttends.test.ts
+++ server/middleware/getWhoAttends.test.ts
@@ -13,10 +13,11 @@ jest.mock('../data/tokenStore/redisTokenStore')
 
 const username = 'user-1'
 
-const mockProvidersResponse = {
-  name: {
-    forename: 'Caroline',
-    surname: 'Wolff',
+const mockAPIResponse = {
+  defaultUserDetails: {
+    username: 'peter-parker',
+    homeArea: 'London',
+    team: 'Automated Allocation Team',
   },
   providers: [
     {
@@ -26,6 +27,53 @@ const mockProvidersResponse = {
     {
       code: 'N07',
       name: 'London',
+    },
+    {
+      code: 'N54',
+      name: 'North East Region',
+    },
+  ],
+  teams: [
+    {
+      description: 'A P Central Admissions Unit',
+      code: 'N50CAU',
+    },
+    {
+      description: 'Ascot House Approved Premises',
+      code: 'N50AHA',
+    },
+    {
+      description: 'Atherton Court',
+      code: 'N50ACT',
+    },
+  ],
+  users: [
+    {
+      username: 'peter-parker',
+      name: 'peter parker (PS-PSO)',
+    },
+    {
+      username: 'jon-smith',
+      name: 'jon smith (PS-PSO)',
+    },
+  ],
+} as any
+
+const session = {
+  defaultUserDetails: {
+    username: 'peter-parker',
+    homeArea: 'London',
+    team: 'Automated Allocation Team',
+  },
+  providers: [
+    {
+      code: 'N50',
+      name: 'Greater Manchester',
+    },
+    {
+      code: 'N07',
+      name: 'London',
+      selected: 'selected',
     },
     {
       code: 'N54',
@@ -74,7 +122,7 @@ describe('/middleware/getWhoAttends()', () => {
 
   const spy = jest
     .spyOn(MasApiClient.prototype, 'getUserProviders')
-    .mockImplementation(() => Promise.resolve(mockProvidersResponse))
+    .mockImplementation(() => Promise.resolve(mockAPIResponse))
 
   afterEach(() => {
     jest.clearAllMocks()
@@ -85,7 +133,7 @@ describe('/middleware/getWhoAttends()', () => {
       session: {
         data: {
           providers: {
-            'user-2': mockProvidersResponse.providers,
+            'user-2': mockAPIResponse.providers,
           },
         },
       },
@@ -94,14 +142,15 @@ describe('/middleware/getWhoAttends()', () => {
       await getWhoAttends(hmppsAuthClient)(req, res, nextSpy)
     })
     it('should fetch the user providers from the api and assign to session', () => {
+      console.log(req.session.data.providers)
       expect(spy).toHaveBeenCalledWith(username, undefined, undefined)
       expect(req.session.data.providers).toEqual({
         ...req.session.data.providers,
-        [username]: mockProvidersResponse.providers,
+        [username]: session.providers,
       })
     })
     it('should assign the user providers to res.locals', () => {
-      expect(res.locals.userProviders).toEqual(mockProvidersResponse.providers)
+      expect(res.locals.userProviders).toEqual(session.providers)
     })
     it('should call next()', () => {
       expect(nextSpy).toHaveBeenCalled()
@@ -112,7 +161,7 @@ describe('/middleware/getWhoAttends()', () => {
       session: {
         data: {
           providers: {
-            [username]: mockProvidersResponse.providers,
+            [username]: mockAPIResponse.providers,
           },
         },
       },
@@ -124,7 +173,7 @@ describe('/middleware/getWhoAttends()', () => {
       expect(spy).toHaveBeenCalledWith(username, undefined, undefined)
       expect(req.session.data.providers).toEqual({
         ...req.session.data.providers,
-        [username]: mockProvidersResponse.providers,
+        [username]: session.providers,
       })
     })
     it('should assign the existing session user providers to res.locals', () => {
