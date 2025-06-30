945ee5b71d484642ca8af7eb891c01f26c578b3d
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-06-30 16:11:04 +0100

    MAN-636 test coverage wip

--- integration_tests/e2e/appointments/ arrange-another-appointment.cy.ts
+++ integration_tests/e2e/appointments/ arrange-another-appointment.cy.ts
@@ -1,4 +1,5 @@
 import ArrangeAnotherAppointmentPage from '../../pages/appointments/arrange-another-appointment.page'
+import AppointmentDateTimePage from '../../pages/appointments/date-time.page'
 import {
   completeTypePage,
   completeSentencePage,
@@ -10,6 +11,9 @@ import {
   completeCYAPage,
   completeConfirmationPage,
   checkAppointmentSummary,
+  checkAppointmentUpdate,
+  crn,
+  uuid,
 } from './imports'
 
 const loadPage = () => {
@@ -33,10 +37,41 @@ describe('Arrange another appointment', () => {
       'contain.text',
       'Use the saved details of your recently created appointment to create a new one. You can amend any of the details.',
     )
-    checkAppointmentSummary(arrangeAnotherAppointmentPage)
+    // checkAppointmentSummary(arrangeAnotherAppointmentPage)
     arrangeAnotherAppointmentPage.getSubmitBtn().should('include.text', 'Arrange appointment')
   })
-  it('should link to the date/time page and display validation errors if the user clicks submit without selecting a schedule', () => {
+  /*
+  describe('User clicks submit without selecting a date and time', () => {
     const arrangeAnotherAppointmentPage = new ArrangeAnotherAppointmentPage()
+    const dateTimePage = new AppointmentDateTimePage()
+    beforeEach(() => {
+      arrangeAnotherAppointmentPage.getSubmitBtn().click()
+    })
+    it('should redirect to the date/time page', () => {
+      dateTimePage.checkOnPage()
+    })
+    it('should display the error summary box', () => {
+      dateTimePage.checkErrorSummaryBox(['Enter or select a date', 'Select a start time', 'Select an end time'])
+    })
+    it('should display the error messages', () => {
+      dateTimePage.getElement(`#appointments-${crn}-${uuid}-date-error`).should($error => {
+        expect($error.text().trim()).to.include('Enter or select a date')
+      })
+      dateTimePage.getElement(`#appointments-${crn}-${uuid}-start-error`).should($error => {
+        expect($error.text().trim()).to.include('Select a start time')
+      })
+      dateTimePage.getElement(`#appointments-${crn}-${uuid}-end-error`).should($error => {
+        expect($error.text().trim()).to.include('Select an end time')
+      })
+    })
+  })
+    */
+
+  describe('Change appointment values', () => {
+    const arrangeAnotherAppointmentPage = new ArrangeAnotherAppointmentPage()
+    beforeEach(() => {
+      loadPage()
+    })
+    checkAppointmentUpdate(arrangeAnotherAppointmentPage)
   })
 })
