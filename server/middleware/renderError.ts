450e626ac7ee5c0afb1d97ffdeb1c9f8eb56913a
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-07-11 16:48:46 +0100

    MAN-719 update renderError function

--- server/middleware/renderError.ts
+++ server/middleware/renderError.ts
@@ -4,7 +4,7 @@ import { StatusErrorCode, statusErrors } from '../properties'
 export const renderError =
   (status: StatusErrorCode) =>
   (_req: Request, res: Response): void => {
-    const errorStatus = statusErrors?.[status] ? status : '500'
+    const errorStatus = statusErrors?.[status] ? status : 500
     const { title, message } = statusErrors[errorStatus]
     res.locals.title = title
     res.locals.message = message
