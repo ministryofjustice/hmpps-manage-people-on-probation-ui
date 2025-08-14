c6f71deaa5df7ba9888f40d6ce369884b808e020
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-08-14 19:41:38 +0100

    MAN-901: remove unused test file

--- integration_tests/pages/upcoming-appointments.ts
+++
@@ -1,9 +0,0 @@
-import Page, { PageElement } from './page'
-
-export default class UpcomingAppointments extends Page {
-  appointmentType = (): PageElement => cy.get('[data-qa=appointmentType]')
-
-  appointmentTitle = (): PageElement => cy.get('[data-qa=appointmentTitle]')
-
-  complianceTag = (): PageElement => cy.get('[data-qa=complianceTag]')
-}
