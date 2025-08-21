3e21259d7482943664a75fef31953d9cffb86ebc
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-08-21 20:25:27 +0100

    MAN-958 add styling overrides for upload feedback list

--- assets/js/index.js
+++ assets/js/index.js
@@ -97,97 +97,149 @@ const attendanceSelectors = () => {
   }
 }
 
+const homeSearch = () => {
+  const search = document.getElementById('homepage-search-suggestions')
+
+  if (search) {
+    search.addEventListener('click', e => {
+      if (e.target.dataset.suggestedQuery) {
+        document.getElementById('search').value = e.target.dataset.suggestedQuery
+        document.getElementById('homepage-search-form').submit()
+      }
+    })
+  }
+}
+
+const multiFileUpload = () => {
+  MOJFrontend.MultiFileUpload.prototype.getSuccessHtml = (success, status = 'Uploaded', color = 'grey') => {
+    return `<span class="moj-multi-file-upload__success"><span class="moj-multi-file-upload__message-text moj-multi-file-upload__message-text--with-icon"><svg class="moj-banner__icon" fill="currentColor" role="presentation" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" height="25" width="25"><path d="M25,6.2L8.7,23.2L0,14.1l4-4.2l4.7,4.9L21,2L25,6.2z"/></svg>${success.messageHtml}</span><strong class="moj-multi-file-upload__message-status"><string class="govuk-tag govuk-tag--${color}">${status}</strong></span>`
+  }
+
+  MOJFrontend.MultiFileUpload.prototype.getErrorHtml = (error, status = 'Upload failed', color = 'red') => {
+    return `<span class="moj-multi-file-upload__error"><span class="moj-multi-file-upload__message-text moj-multi-file-upload__message-text--with-icon"><svg class="moj-banner__icon" fill="currentColor" role="presentation" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" height="25" width="25"><path d="M13.6,15.4h-2.3v-4.5h2.3V15.4z M13.6,19.8h-2.3v-2.2h2.3V19.8z M0,23.2h25L12.5,2L0,23.2z"/></svg>${error.message}</span><strong class="moj-multi-file-upload__message-status"><string class="govuk-tag govuk-tag--${color}">${status}</strong></span>`
+  }
+
+  MOJFrontend.MultiFileUpload.prototype.getFileRowHtml = file => {
+    const html = `
+      <div class="govuk-summary-list__row moj-multi-file-upload__row">
+        <div class="govuk-summary-list__value moj-multi-file-upload__message">
+        <div class="moj-multi-file-upload__message-text">
+      <span class="moj-multi-file-upload__filename">${file.name}</span>
+      <span class="moj-multi-file-upload__progress visibility-hidden">0%</span>
+      </div>
+      <strong class="moj-multi-file-upload__message-status"><string class="govuk-tag govuk-tag--yellow">Uploading</strong>
+        </div>
+        <div class="govuk-summary-list__actions moj-multi-file-upload__actions"></div>
+      </div>`
+    return html
+  }
+
+  const multiUpload = document.querySelector('.moj-multi-file-upload')
+  if (typeof MOJFrontend.MultiFileUpload !== 'undefined' && multiUpload) {
+    new MOJFrontend.MultiFileUpload({
+      container: $('.moj-multi-file-upload'),
+      uploadUrl: '/appointments/file/upload',
+      deleteUrl: '/appointments/file/delete',
+      dropzoneHintText: 'Drag and drop files here or ',
+      headers: {
+        'CSRF-Token': window.csrfToken,
+      },
+      // uploadFileEntryHook: uploadEntry,
+      uploadFileExitHook: uploadExit,
+      fileDeleteHook: deleteExit,
+      uploadFileErrorHook: errorHook,
+    })
+
+    function continueButtonEnabled(enabled) {
+      const $continueButton = $('#continueButton')
+      if (enabled) {
+        $continueButton.removeAttr('disabled')
+        $continueButton.removeAttr('aria-disabled')
+        $continueButton.removeClass('govuk-button--disabled')
+      } else {
+        $continueButton.attr('disabled', 'disabled')
+        $continueButton.attr('aria-disabled', 'true')
+        $continueButton.addClass('govuk-button--disabled')
+      }
+    }
+
+    // Uploading finished when all delete buttons have appeared
+    function isAllFinished() {
+      const $actions = $('.moj-multi-file-upload__actions')
+      let finished = true
+      $actions.each(() => {
+        if ($(this).html() === '') {
+          finished = false
+        }
+      })
+      return finished
+    }
+
+    // function uploadEntry(_handle, _file, _response) {
+    //   // eslint-disable-next-line no-debugger
+    //   debugger
+    // }
+
+    function uploadExit(_handle, _file, _response) {
+      // const $actions = $('.moj-multi-file-upload__actions')
+
+      if (isAllFinished()) {
+        continueButtonEnabled(true)
+      } else if ($('.moj-multi-file-upload__list')) {
+        // another file has been added
+        continueButtonEnabled(false)
+      }
+    }
+
+    function deleteExit(_handle, response) {
+      const $row = $('.moj-multi-file-upload__row')
+      if ($row && $row.length === 0) {
+        continueButtonEnabled(false)
+      }
+    }
+
+    function errorHook(handle, file, jqXHR, textStatus, errorThrown) {
+      let message = ''
+      switch (jqXHR.status) {
+        case 413:
+          message = `${file.name}: File size must be 5mb or under`
+          break
+        case 415:
+          message = `${file.name}: file type must be pdf or word`
+          break
+        default:
+          message = `${file.name}: ${textStatus || errorThrown}`
+      }
+      const item = $(handle.getFileRowHtml(file))
+      item.find('.moj-multi-file-upload__message').html(handle.getErrorHtml({ message }, 'Upload failed', 'red'))
+      item
+        .find('.moj-multi-file-upload__actions')
+        .append(handle.getDeleteButtonHtml({ filename: file.name }, 'Upload failed', 'red'))
+      const $row = handle.feedbackContainer.find(`.moj-multi-file-upload__row:contains("${file.name}")`)
+      $row.replaceWith(item.prop('outerHTML'))
+      handle.status.html(message)
+    }
+  }
+}
+
+const crissHeaders = () => {
+  const btn = document.getElementById('crissButton')
+  if (btn) {
+    const textarea = document.getElementById('notes')
+    const status = document.getElementById('crissStatus')
+    btn.addEventListener('click', () => {
+      if (textarea.value.trim() === '') {
+        textarea.value = `Check in\n\nReview\n\nIntervention\n\nSummarise\n\nSet tasks`
+        status.textContent = 'CRISS headers inserted'
+      }
+    })
+  }
+}
+
 setNoFixedAddressConditional()
 lastAppointment()
 resetConditionals()
 attendanceSelectors()
-
-const search = document.getElementById('homepage-search-suggestions')
-
-if (search) {
-  search.addEventListener('click', e => {
-    if (e.target.dataset.suggestedQuery) {
-      document.getElementById('search').value = e.target.dataset.suggestedQuery
-      document.getElementById('homepage-search-form').submit()
-    }
-  })
-}
-
-const multiUpload = document.querySelector('.moj-multi-file-upload')
-if (typeof MOJFrontend.MultiFileUpload !== 'undefined' && multiUpload) {
-  new MOJFrontend.MultiFileUpload({
-    container: $('.moj-multi-file-upload'),
-    uploadUrl: '/appointments/file/upload',
-    deleteUrl: '/appointments/file/delete',
-    dropzoneHintText: 'Drag and drop files here or ',
-    headers: {
-      'CSRF-Token': window.csrfToken,
-    },
-    uploadFileExitHook: uploadExit,
-    fileDeleteHook: deleteExit,
-    uploadFileErrorHook: errorHook,
-  })
-
-  function continueButtonEnabled(enabled) {
-    const $continueButton = $('#continueButton')
-    if (enabled) {
-      $continueButton.removeAttr('disabled')
-      $continueButton.removeAttr('aria-disabled')
-      $continueButton.removeClass('govuk-button--disabled')
-    } else {
-      $continueButton.attr('disabled', 'disabled')
-      $continueButton.attr('aria-disabled', 'true')
-      $continueButton.addClass('govuk-button--disabled')
-    }
-  }
-
-  // Uploading finished when all delete buttons have appeared
-  function isAllFinished() {
-    const $actions = $('.moj-multi-file-upload__actions')
-    let finished = true
-    $actions.each(() => {
-      if ($(this).html() === '') {
-        finished = false
-      }
-    })
-    return finished
-  }
-
-  function uploadExit(_handle, _file, _response) {
-    // const $actions = $('.moj-multi-file-upload__actions')
-
-    if (isAllFinished()) {
-      continueButtonEnabled(true)
-    } else if ($('.moj-multi-file-upload__list')) {
-      // another file has been added
-      continueButtonEnabled(false)
-    }
-  }
-
-  function deleteExit(_handle, response) {
-    const $row = $('.moj-multi-file-upload__row')
-    if ($row && $row.length === 0) {
-      continueButtonEnabled(false)
-    }
-  }
-
-  function errorHook(handle, file, jqXHR, textStatus, errorThrown) {
-    let message = ''
-    switch (jqXHR.status) {
-      case 413:
-        message = `${file.name}: File size must be 5mb or under`
-        break
-      case 415:
-        message = `${file.name}: file type must be pdf or word`
-        break
-      default:
-        message = `${file.name}: ${textStatus || errorThrown}`
-    }
-    const item = $(handle.getFileRowHtml(file))
-    item.find('.moj-multi-file-upload__message').html(handle.getErrorHtml({ message }))
-    item.find('.moj-multi-file-upload__actions').append(handle.getDeleteButtonHtml({ filename: file.name }))
-    const $row = handle.feedbackContainer.find(`.moj-multi-file-upload__row:contains("${file.name}")`)
-    $row.replaceWith(item.prop('outerHTML'))
-    handle.status.html(message)
-  }
-}
+homeSearch()
+multiFileUpload()
+crissHeaders()
