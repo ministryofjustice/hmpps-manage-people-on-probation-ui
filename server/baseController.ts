a803fd8bc435ec20eb2ef2da316b84443fd29544
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-09-26 13:53:27 +0100

    MAN-1193 back link fix wip

--- server/baseController.ts
+++ server/baseController.ts
@@ -12,6 +12,15 @@ const baseController = () => {
     res.locals.cases = url[0] === 'case'
     res.locals.search = url[0] === 'search'
     res.locals.makePageTitle = makePageTitle
+    let backLink: string
+    if (req?.session?.backLink) {
+      backLink = req.session.backLink
+      delete req?.session?.backLink
+    } else {
+      const { change, back } = req.query as Record<string, string>
+      backLink = change || back || ''
+    }
+    res.locals.backLink = backLink
     return next()
   }
 }
