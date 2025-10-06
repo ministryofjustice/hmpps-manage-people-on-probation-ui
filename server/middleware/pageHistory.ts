c4fdf958bb29379c2e23bdaeeb0e7c04e924efd7
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-10-06 11:40:24 +0100

    Add override for back url query param

--- server/middleware/pageHistory.ts
+++ server/middleware/pageHistory.ts
@@ -16,6 +16,9 @@ export const pageHistory = (req: Request, res: Response, _next?: NextFunction) =
     } else {
       history = [url]
     }
+    if (req?.query?.back) {
+      history = [...history.slice(0, history.length - 2), req.query.back as string, ...history.slice(-1)]
+    }
     res.locals.backLink = history?.[history.length - 2] ?? null
     req.session.pageHistory = history
   }
