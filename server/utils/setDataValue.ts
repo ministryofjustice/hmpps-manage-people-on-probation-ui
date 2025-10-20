4327f873961aa32c03f9fe08da8ba2ad1942e0cc
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2025-10-20 13:40:23 +0100

    MAN-1298 - Updated the setDataValue.ts to remove lodash

--- server/utils/setDataValue.ts
+++ server/utils/setDataValue.ts
@@ -1,6 +1,14 @@
-import setKeypath from 'lodash/set'
-
 export const setDataValue = (data: any, sections: any, value: any) => {
-  const path = Array.isArray(sections) ? sections : [sections]
-  return setKeypath(data, path.map((s: any) => `["${s}"]`).join(''), value)
+  const path = Array.isArray(sections) ? sections.map(String) : [String(sections)]
+
+  let target = data
+  for (let i = 0; i < path.length - 1; i += 1) {
+    const key = path[i]
+    if (typeof target?.[key] !== 'object' || target?.[key] === null || Array.isArray(target?.[key])) {
+      target[key] = {}
+    }
+    target = target[key]
+  }
+  target[path[path.length - 1]] = value
+  return data
 }
