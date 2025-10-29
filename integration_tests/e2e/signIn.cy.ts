7a30c148809224f5df11293e42f18ffe4f84d800
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2025-10-29 10:00:20 +0000

    fix for failed test

--- integration_tests/e2e/signIn.cy.ts
+++ integration_tests/e2e/signIn.cy.ts
@@ -22,8 +22,8 @@ context('Sign In', () => {
   it('User can sign out', () => {
     cy.visit('/')
     const indexPage = Page.verifyOnPage(IndexPage)
-    indexPage.menuButtonBySpan().eq(1).should('be.visible').click()
-    indexPage.feSignOut().eq(1).should('be.visible').click()
+    indexPage.menuButton().should('be.visible').click()
+    indexPage.feSignOut().contains('Sign out').should('be.visible').click()
     Page.verifyOnPage(AuthSignInPage)
   })
 })
