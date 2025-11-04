b5b7451a35e9f291a505cf5a23322e56ab3d9e14
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-11-04 12:59:50 +0000

    MAN-1404 failing test fixes

--- server/utils/convertToTitleCase.ts
+++ server/utils/convertToTitleCase.ts
@@ -5,14 +5,15 @@ export const properCase = (word: string): string =>
 
 export const properCaseName = (name: string): string => (isBlank(name) ? '' : name.split('-').map(properCase).join('-'))
 
-const regex = /\(([A-Z]+(?:[- ][A-Z]+)*)\)/g
-
-export const convertToTitleCase = (sentence: string, ignore: string[] = []): string => {
+export const convertToTitleCase = (sentence: string, ignore: string[] = [], regex?: RegExp): string => {
   if (isBlank(sentence)) return ''
   return sentence
     .split(' ')
     .map(word => {
-      if (ignore.includes(word) || regex.test(word)) {
+      if (ignore.includes(word)) {
+        return word
+      }
+      if (regex && regex.test(word)) {
         return word
       }
       return properCaseName(word)
