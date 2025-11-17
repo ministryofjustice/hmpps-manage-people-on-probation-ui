92a27ff872efe527021c88badd4ce77ca4c4c223
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2025-11-17 18:05:25 +0000

    added instructions page

--- server/routes/check-ins.ts
+++ server/routes/check-ins.ts
@@ -3,6 +3,5 @@ import type { Services } from '../services'
 import controllers from '../controllers'
 
 export default function checkInsRoutes(router: Router, { hmppsAuthClient }: Services) {
-
   router.get('/case/:crn/appointments/check-in/instructions', [controllers.checkIns.getIntroPage(hmppsAuthClient)])
 }
