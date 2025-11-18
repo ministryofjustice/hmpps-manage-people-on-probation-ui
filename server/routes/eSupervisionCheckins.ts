8261742ddf21c03990115d7b62c58b591b50838f
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2025-11-18 11:13:19 +0000

    changed route name

--- server/routes/eSupervisionCheckins.ts
+++ server/routes/eSupervisionCheckins.ts
@@ -2,6 +2,6 @@ import { type Router } from 'express'
 import type { Services } from '../services'
 import controllers from '../controllers'
 
-export default function checkInsRoutes(router: Router, { hmppsAuthClient }: Services) {
+export default function eSuperVisionCheckInsRoutes(router: Router, { hmppsAuthClient }: Services) {
   router.get('/case/:crn/appointments/check-in/instructions', [controllers.checkIns.getIntroPage(hmppsAuthClient)])
 }
