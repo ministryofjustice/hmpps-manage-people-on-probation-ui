0eab6ca5a5b04758ac8fa19af9cd088465e5b839
rav-moj <ravindrakumar.killamsetty@justice.gov.uk>
2025-11-19 13:47:40 +0000

    MAN-1483: Add checkin frequency page (#906)
    * add checkin frequency page
    
    * add test
    
    * added new middleware
    
    * removed old changes
    
    * removed old changes
    
    * removed old changes

---
+++ integration_tests/pages/check-ins/contact-preference.ts
@@ -0,0 +1,7 @@
+import Page from '../page'
+
+export default class ContactPreferencePage extends Page {
+  constructor() {
+    super('Contact Preferences')
+  }
+}
--- server/controllers/check-ins.ts
+++ server/controllers/check-ins.ts
@@ -1,8 +1,16 @@
+import { v4 as uuidv4 } from 'uuid'
+import { DateTime } from 'luxon'
 import { Controller } from '../@types'
-import { isValidCrn } from '../utils'
+import { isValidCrn, isValidUUID } from '../utils'
 import { renderError } from '../middleware'
 
-const routes = ['getIntroPage'] as const
+const routes = [
+  'getIntroPage',
+  'getDateFrequencyPage',
+  'postIntroPage',
+  'postDateFrequencyPage',
+  'getContactPreferencePage',
+] as const
 
 const checkInsController: Controller<typeof routes, void> = {
   getIntroPage: hmppsAuthClient => {
@@ -12,6 +20,57 @@ const checkInsController: Controller<typeof routes, void> = {
         return renderError(404)(req, res)
       }
       return res.render('pages/check-in/instructions.njk', { crn })
+    }
+  },
+
+  postIntroPage: hmppsAuthClient => {
+    return async (req, res) => {
+      const { crn } = req.params
+      if (!isValidCrn(crn)) {
+        return renderError(404)(req, res)
+      }
+      const id = uuidv4()
+      return res.redirect(`/case/${crn}/appointments/${id}/check-in/date-frequency`)
+    }
+  },
+
+  getDateFrequencyPage: hmppsAuthClient => {
+    return async (req, res) => {
+      const { crn, id } = req.params
+      if (!isValidCrn(crn) || !isValidUUID(id)) {
+        return renderError(404)(req, res)
+      }
+      const today = new Date()
+      // setting temporary fix for minDate
+      // (https://github.com/ministryofjustice/moj-frontend/issues/923)
+      let checkInMinDate: string
+      today.setDate(today.getDate() + 1)
+      if (today.getDate() > 9) {
+        checkInMinDate = DateTime.fromJSDate(today).toFormat('dd/M/yyyy')
+      } else {
+        checkInMinDate = DateTime.fromJSDate(today).toFormat('d/M/yyyy')
+      }
+      return res.render(`pages/check-in/date-frequency.njk`, { crn, id, checkInMinDate })
+    }
+  },
+
+  postDateFrequencyPage: hmppsAuthClient => {
+    return async (req, res) => {
+      const { crn, id } = req.params
+      if (!isValidCrn(crn) || !isValidUUID(id)) {
+        return renderError(404)(req, res)
+      }
+      return res.redirect(`/case/${crn}/appointments/${id}/check-in/contact-preference`)
+    }
+  },
+
+  getContactPreferencePage: hmppsAuthClient => {
+    return async (req, res) => {
+      const { crn, id } = req.params
+      if (!isValidCrn(crn) || !isValidUUID(id)) {
+        return renderError(404)(req, res)
+      }
+      return res.render('pages/check-in/contact-preference.njk', { crn, id })
     }
   },
 }
