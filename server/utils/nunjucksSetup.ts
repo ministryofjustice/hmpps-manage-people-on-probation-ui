e3dd6305d36d82440374879d67437d52acd2dcbc
Aidan Filby <Aidan.Filby@tpximpact.com>
2025-12-02 12:20:33 +0000

    provide formatting from esupervision

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -73,6 +73,8 @@ import {
 import { ApplicationInfo } from '../applicationInfo'
 import config from '../config'
 import { AppResponse } from '../models/Locals'
+import { splitString } from './split'
+import getUserFriendlyString from './eSupervisionFriendlyString'
 
 export default function nunjucksSetup(app: express.Express, applicationInfo: ApplicationInfo): void {
   const production = process.env.NODE_ENV === 'production'
@@ -138,6 +140,8 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   njkEnv.addFilter('toSentenceDescription', toSentenceDescription)
   njkEnv.addFilter('concat', concat)
   njkEnv.addFilter('shortTime', shortTime)
+  njkEnv.addFilter('split', splitString)
+  njkEnv.addFilter('userFriendlyString', getUserFriendlyString)
   njkEnv.addFilter('convertToTitleCase', convertToTitleCase)
   njkEnv.addFilter('handleQuotes', handleQuotes)
   njkEnv.addFilter('dmyToLongDate', dateToLongDate)
