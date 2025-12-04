4af0e9dcdb0107d1eb2f95eae38caa9c1c5b0c01
Neil Mills <neil.mills@digital.justice.gov.uk>
2025-12-04 14:13:56 +0000

    MAN-1340 fix attended complied banner content

--- integration_tests/e2e/appointments/imports.ts
+++ integration_tests/e2e/appointments/imports.ts
@@ -443,18 +443,25 @@ export const checkUpdateRepeating = (page: AppointmentCheckYourAnswersPage | Arr
   })
 }
 
-export const checkLogOutcomesAlert = () => {
+export const checkLogOutcomesAlert = (attendedComplied = false) => {
   it('should render the log outcomes alert banner', () => {
     cy.get('[data-module="serviceAlert"]').as('alert')
     cy.get('@alert')
       .get('.moj-alert__content')
-      .should('contain.text', 'You can only use this service to log attended and complied outcomes.')
-    cy.get('@alert')
-      .get('.moj-alert__content')
-      .should('contain.text', 'You can only use this service to log attended and complied outcomes.')
+      .should(
+        'contain.text',
+        !attendedComplied
+          ? 'You can only use this service to log attended and complied outcomes.'
+          : 'You can only log attended and complied outcomes. If you need to log a different outcome,',
+      )
     cy.get('@alert')
       .get('.moj-alert__content a')
-      .should('contain.text', 'Use NDelius to arrange an appointment in the past with another outcome')
+      .should(
+        'contain.text',
+        !attendedComplied
+          ? 'Use NDelius to arrange an appointment in the past with another outcome'
+          : 'arrange this appointment on NDelius',
+      )
       .should('have.attr', 'href', '#')
       .should('have.attr', 'target', '_blank')
     cy.get('@alert').get('.moj-alert__action button').should('contain.text', 'Dismiss')
