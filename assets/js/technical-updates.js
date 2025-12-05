b843373ce49e9f05a692f4f0c48996cfdfb4ac19
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2025-12-05 17:19:06 +0000

    MAN-1401 - eslint function name

--- assets/js/technical-updates.js
+++ assets/js/technical-updates.js
@@ -1,11 +1,11 @@
 const technicalUpdatesBanner = this.document.getElementById('technical-updates-banner')
 
-this.document.getElementById('hide-message').addEventListener('click', function () {
+this.document.getElementById('hide-message').addEventListener('click', function handleHideMessageClick() {
   localStorage.setItem('technicalUpdateReadVersion', technicalUpdatesBanner.dataset.bannerVersion)
   technicalUpdatesBanner.classList.add('moj-hidden')
 })
 
-window.addEventListener('load', function () {
+window.addEventListener('load', function handleWindowLoad() {
   const { technicalUpdateReadVersion } = localStorage
   const currentBannerVersion = technicalUpdatesBanner.dataset.bannerVersion
 
