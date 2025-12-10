ecc07bd87f62bbe8bc32a2707bafddb114337d71
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2025-12-10 14:00:29 +0000

    MAN-1401 - test fix

--- integration_tests/pages/appointments/check-your-answers.page.ts
+++ integration_tests/pages/appointments/check-your-answers.page.ts
@@ -3,6 +3,5 @@ import Page from '../page'
 export default class AppointmentCheckYourAnswersPage extends Page {
   constructor() {
     super(`Check your answers`)
-    this.hideMessageLink().click()
   }
 }
