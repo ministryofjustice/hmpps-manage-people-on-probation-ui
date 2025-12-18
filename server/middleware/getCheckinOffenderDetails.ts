9cc2c835462acd7b8a12bad046e651259b71d6d2
rav-moj <ravindrakumar.killamsetty@justice.gov.uk>
2025-12-18 17:15:17 +0000

    feature flag esup api call (#982)

--- server/middleware/getCheckinOffenderDetails.ts
+++ server/middleware/getCheckinOffenderDetails.ts
@@ -7,12 +7,15 @@ import { OffenderCheckinsByCRNResponse } from '../data/model/esupervision'
 export const getCheckinOffenderDetails = (hmppsAuthClient: HmppsAuthClient): Route<Promise<void>> => {
   return async (req, res, next) => {
     const { crn } = req.params
-    const token = await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
-    const eSupervisionClient = new ESupervisionClient(token)
-    const offenderCheckinsByCRNResponse: OffenderCheckinsByCRNResponse =
-      await eSupervisionClient.getOffenderCheckinsByCRN(crn)
-    if (offenderCheckinsByCRNResponse?.status === 'VERIFIED') {
-      res.locals.offenderCheckinsByCRNResponse = offenderCheckinsByCRNResponse
+    const { enableESuperVision } = res.locals.flags
+    if (enableESuperVision) {
+      const token = await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
+      const eSupervisionClient = new ESupervisionClient(token)
+      const offenderCheckinsByCRNResponse: OffenderCheckinsByCRNResponse =
+        await eSupervisionClient.getOffenderCheckinsByCRN(crn)
+      if (offenderCheckinsByCRNResponse?.status === 'VERIFIED') {
+        res.locals.offenderCheckinsByCRNResponse = offenderCheckinsByCRNResponse
+      }
     }
     return next()
   }
