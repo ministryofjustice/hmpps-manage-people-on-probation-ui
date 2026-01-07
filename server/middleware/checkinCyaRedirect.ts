4073d31c88f00f1107fba9c66bb7cb8f9ef1832b
rav-moj <ravindrakumar.killamsetty@justice.gov.uk>
2026-01-07 09:53:26 +0000

    MAN-1611: Amended contact pref page (#1009)
    * Amended contact pref page
    
    * Amended contact pref page
    
    * Amended contact pref page
    
    * Addressed review comments

---
+++ server/middleware/checkinCyaRedirect.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express'
+import { Route } from '../@types'
+
+export const redirectWizard = (): Route<Promise<void>> => {
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { crn, id } = req.params
+    const redirectUrl = `/case/${crn}/appointments/${id}/check-in/checkin-summary`
+    const skipContactPrefTypes = ['mobile', 'emailAddress']
+    if (req.query.cya === 'true') {
+      if (skipContactPrefTypes.includes(req.body.change)) {
+        return next()
+      }
+      return res.redirect(redirectUrl)
+    }
+    return next()
+  }
+}
