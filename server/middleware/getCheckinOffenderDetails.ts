2f6343f84fb45cbbd744c6b861195501174c3f40
rav-moj <ravindrakumar.killamsetty@justice.gov.uk>
2026-01-19 09:53:33 +0000

    Remove generic call (#1044)

--- server/middleware/getCheckinOffenderDetails.ts
+++ server/middleware/getCheckinOffenderDetails.ts
@@ -1,18 +1,15 @@
 import { HmppsAuthClient } from '../data'
 import { Route } from '../@types'
-
 import ESupervisionClient from '../data/eSupervisionClient'
-import { OffenderCheckinsByCRNResponse } from '../data/model/esupervision'
 
 export const getCheckinOffenderDetails = (hmppsAuthClient: HmppsAuthClient): Route<Promise<void>> => {
   return async (req, res, next) => {
     const { crn } = req.params
-    const { enableESuperVision } = res.locals.flags
+    const enableESuperVision = res.locals?.flags?.enableESuperVision ?? false
     if (enableESuperVision) {
       const token = await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
       const eSupervisionClient = new ESupervisionClient(token)
       res.locals.offenderCheckinsByCRNResponse = await eSupervisionClient.getOffenderCheckinsByCRN(crn)
     }
-    return next()
   }
 }
