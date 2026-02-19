c2535f4e3a389d3be2d32f23d572ca006ab7dc6e
Neil Mills <35219002+neil-mills@users.noreply.github.com>
2026-02-19 12:52:05 +0000

    MAN-1972 bug fix (#1134)

--- server/middleware/routeChangeAttendee.ts
+++ server/middleware/routeChangeAttendee.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
+import { setDataValue } from '../utils'
 
 export const routeChangeAttendee = (req: Request, res: Response, next: NextFunction) => {
   const {
@@ -9,6 +10,9 @@ export const routeChangeAttendee = (req: Request, res: Response, next: NextFunct
   } = req
   if (body?.['submit-btn'] === '') {
     return next()
+  }
+  if (req?.body?.appointments?.[crn]?.[id]?.type) {
+    setDataValue(req.session.data, ['appointments', crn, id, 'type'], req.body.appointments[crn][id].type)
   }
   const redirectUrl = `/case/${crn}/arrange-appointment/${id}/attendance${change ? `?change=${encodeURIComponent(url)}` : ''}`
   return res.redirect(redirectUrl)
