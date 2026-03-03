af9a4d75b9775fa74af73186515b2024fb7cd76c
Neil Mills <neil.mills@digital.justice.gov.uk>
2026-03-03 18:35:00 +0000

    MAN-2115 use component from repo

--- server/utils/toPredictors.test.ts
+++ server/utils/toPredictors.test.ts
@@ -1,49 +1,55 @@
 import { toPredictors } from './toPredictors'
 import { riskScores as predictorScores } from './mocks'
-import { RiskScoresDto, TimelineItem } from '../data/model/risk'
+import { RiskScoresDto, TimelineItem, ScoreType } from '../data/model/risk'
 
 const getExpectedScores = (riskScore: RiskScoresDto) => {
   return {
     RSR: {
-      type: 'RSR',
+      type: 'Combined Serious Reoffending Predictor',
       level: riskScore.riskOfSeriousRecidivismScore?.scoreLevel,
       score: riskScore.riskOfSeriousRecidivismScore?.percentageScore,
+      staticOrDynamic: 'Dynamic' as ScoreType,
     },
     OGP: {
-      type: 'OGP',
+      type: 'OASys General Predictor Score',
       level: riskScore.generalPredictorScore?.ogpRisk,
       oneYear: riskScore.generalPredictorScore?.ogp1Year,
       twoYears: riskScore.generalPredictorScore?.ogp2Year,
+      staticOrDynamic: 'Dynamic' as ScoreType,
     },
     OSPC: {
-      type: 'OSP/C',
+      type: 'Direct Contact - Sexual Reoffending Predictor',
       level:
         riskScore.sexualPredictorScore?.ospContactScoreLevel ||
         riskScore.sexualPredictorScore?.ospDirectContactScoreLevel,
       score:
         riskScore.sexualPredictorScore?.ospContactPercentageScore ||
         riskScore.sexualPredictorScore?.ospDirectContactPercentageScore,
+      staticOrDynamic: 'Static' as ScoreType,
     },
     OSPI: {
-      type: 'OSP/I',
+      type: 'Images and Indirect Contact - Sexual Reoffending Predictor',
       level:
         riskScore.sexualPredictorScore?.ospIndecentScoreLevel ||
         riskScore.sexualPredictorScore?.ospIndirectImageScoreLevel,
       score:
         riskScore.sexualPredictorScore?.ospIndecentPercentageScore ||
         riskScore.sexualPredictorScore?.ospIndirectImagePercentageScore,
+      staticOrDynamic: 'Static' as ScoreType,
     },
     OGRS: {
-      type: 'OGRS',
+      type: 'All Reoffending Predictor',
       level: riskScore.groupReconvictionScore?.scoreLevel,
       oneYear: riskScore.groupReconvictionScore?.oneYear,
       twoYears: riskScore.groupReconvictionScore?.twoYears,
+      staticOrDynamic: 'Static' as ScoreType,
     },
     OVP: {
-      type: 'OVP',
+      type: 'Violent Reoffending Predictor',
       level: riskScore.violencePredictorScore?.ovpRisk,
       oneYear: riskScore.violencePredictorScore?.oneYear,
       twoYears: riskScore.violencePredictorScore?.twoYears,
+      staticOrDynamic: 'Dynamic' as ScoreType,
     },
   }
 }
