8ec9ec43aec86caea3469fe0c89afa7686da8289
rav-moj <ravindrakumar.killamsetty@justice.gov.uk>
2026-03-11 14:19:30 +0000

    MTRSB-25: Remove enableAppointmentCreate flag (#1222)

--- server/middleware/evaluateFeatureFlags.test.ts
+++ server/middleware/evaluateFeatureFlags.test.ts
@@ -13,7 +13,6 @@ const mockFlags = {
   enableNavActivityLog: true,
   enableNavCompliance: true,
   enableNavInterventions: true,
-  enableAppointmentCreate: true,
   enableContactLog: true,
 }
 jest.mock('../services/flagService')
