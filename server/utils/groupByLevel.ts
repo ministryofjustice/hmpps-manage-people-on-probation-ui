115568d36f7767a5e06261b09ad413429eb85ab7
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-03-17 15:19:58 +0000

    remove logs

--- server/utils/groupByLevel.ts
+++ server/utils/groupByLevel.ts
@@ -5,20 +5,10 @@ export const groupByLevel = (level: string | undefined, data: Need[] | RiskFlag[
   if (!data) {
     return []
   }
-  console.log(data)
-  console.log(level)
   const targetLevel = level?.toLowerCase()
   if (isRisk(data)) {
-    console.log(data.filter(item => item?.severity === level))
     return data.filter(item => item?.severity === level)
   }
-  console.log(
-    data.filter(item =>
-      item?.levelDescription
-        ? item.levelDescription.toLowerCase() === targetLevel
-        : item.level?.toLowerCase() === targetLevel,
-    ),
-  )
   return data.filter(item =>
     item?.levelDescription
       ? item.levelDescription.toLowerCase() === targetLevel
