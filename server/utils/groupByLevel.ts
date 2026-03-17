2e7302b195ef089207a852448b2f55f961a8d398
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-03-17 14:45:55 +0000

    N/A for flags with no level

--- server/utils/groupByLevel.ts
+++ server/utils/groupByLevel.ts
@@ -1,14 +1,24 @@
 import { Need, RiskFlag } from '../data/model/risk'
 import { isRisk } from './isRisk'
 
-export const groupByLevel = (level: string, data: Need[] | RiskFlag[]) => {
+export const groupByLevel = (level: string | undefined, data: Need[] | RiskFlag[]) => {
   if (!data) {
     return []
   }
-  const targetLevel = level.toLowerCase()
+  console.log(data)
+  console.log(level)
+  const targetLevel = level?.toLowerCase()
   if (isRisk(data)) {
+    console.log(data.filter(item => item?.severity === level))
     return data.filter(item => item?.severity === level)
   }
+  console.log(
+    data.filter(item =>
+      item?.levelDescription
+        ? item.levelDescription.toLowerCase() === targetLevel
+        : item.level?.toLowerCase() === targetLevel,
+    ),
+  )
   return data.filter(item =>
     item?.levelDescription
       ? item.levelDescription.toLowerCase() === targetLevel
