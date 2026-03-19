1db87f9b05f591472db1776052cca39cdec5839a
Neil Mills <neil.mills@digital.justice.gov.uk>
2026-03-19 12:01:36 +0000

    MAN-2066 manage appointment page updates wip

--- wiremock/stubs/flags.ts
+++ wiremock/stubs/flags.ts
@@ -313,8 +313,8 @@ const stubDisableOGRS4 = (): SuperAgentRequest =>
         flags: [
           ...flags.mappings[0].response.jsonBody.flags,
           {
-            key: 'enableOGRS4',
-            name: 'enableOGRS4',
+            key: 'enableNonCompliance',
+            name: 'enableNonCompliance',
             description: '',
             enabled: false,
             type: 'BOOLEAN_FLAG_TYPE',
