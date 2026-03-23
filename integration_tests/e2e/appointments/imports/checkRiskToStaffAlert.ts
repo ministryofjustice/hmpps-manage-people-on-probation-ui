f3256bd3d48fd1a29ecbf14245f6a206d7cb2d1d
Aidan Filby <146082039+AidanFilby@users.noreply.github.com>
2026-03-23 13:33:40 +0000

    MAN-2193-id-fix (#1263)
    * MAN-2193-id-fix
    
    * cypress test fixes

--- integration_tests/e2e/appointments/imports/checkRiskToStaffAlert.ts
+++ integration_tests/e2e/appointments/imports/checkRiskToStaffAlert.ts
@@ -1,8 +1,29 @@
-export const checkRiskToStaffAlert = (_crn = 'X778160', name = 'Alton', riskLevel = 'very high') => {
+import { riskLevelLabel } from '../../../../server/utils'
+
+export const checkRiskToStaffAlert = (
+  _crn = 'X778160',
+  name = 'Alton',
+  riskLevel?: string,
+  probationStaff?: boolean,
+) => {
   cy.get('[data-qa=riskToStaffAlert]').should('be.visible')
-  cy.get('[data-qa=riskToStaffAlert]').find('h2').should('contain.text', `${name} may be a risk to probation staff`)
-  cy.get('[data-qa=riskToStaffAlert]')
-    .find('a')
-    .should('contain.text', `View ${name}'s ${riskLevel} risk to staff flag`)
-    .should('have.attr', 'href', `/case/${_crn}/risk/flag/1`)
+  if (!probationStaff) {
+    cy.get('[data-qa=riskToStaffAlert]').find('h2').should('contain.text', `${name} may be a risk to probation staff`)
+  } else {
+    cy.get('[data-qa=riskToStaffAlert]').find('h2').should('contain.text', `${name} is a risk to probation staff`)
+    cy.get('[data-qa=riskToStaffAlert]')
+      .find('a')
+      .eq(0)
+      .should('contain.text', `View ${name}'s risk to probation staff flag`)
+      .should('have.attr', 'href')
+      .and('match', new RegExp(`/case/${_crn}/risk/flag/`))
+  }
+  if (riskLevel) {
+    cy.get('[data-qa=riskToStaffAlert]')
+      .find('a')
+      .eq(probationStaff ? 1 : 0)
+      .should('contain.text', `View ${name}'s ${riskLevel} risk to staff flag`)
+      .should('have.attr', 'href')
+      .and('match', new RegExp(`/case/${_crn}/risk/flag/`))
+  }
 }
