962f46e43bd61a51b89d5a360cb5a064c67e8529
Aidan Filby <146082039+AidanFilby@users.noreply.github.com>
2026-03-25 12:33:43 +0000

    MAN-2193 fixes (#1276)
    * MAN-2193 very high can exist
    
    * MAN-2193 ignore level for probation staff flag
    
    * lint fix
    
    * account for no risks

--- server/middleware/getPersonRiskFlags.ts
+++ server/middleware/getPersonRiskFlags.ts
@@ -39,6 +39,15 @@ export const getPersonRiskFlags = (hmppsAuthClient: HmppsAuthClient): Route<Prom
         : null
     res.locals.riskToProbationStaff = riskToProbationStaff ? { id: riskToProbationStaff?.id } : undefined
     res.locals.riskToStaff = riskToStaff ? { id: riskToStaff?.id, level } : undefined
+    personRisks.riskFlags = personRisks?.riskFlags?.map(item => {
+      if (item.description === 'Risk to Probation Staff') {
+        return {
+          ...item,
+          level: undefined,
+        }
+      }
+      return item
+    })
     res.locals.personRisks = personRisks
     return next()
   }
