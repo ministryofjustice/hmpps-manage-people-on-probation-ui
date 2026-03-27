33217700e72d74ccb8ba092cb194bcd2de10b338
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-03-27 17:10:11 +0000

    full test pack

--- cypress.config.ts
+++ cypress.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     },
     baseUrl: 'http://localhost:3007',
     excludeSpecPattern: '**/!(*.cy).ts',
-    specPattern: 'integration_tests/e2e/personalDetails/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'integration_tests/support/index.ts',
     experimentalRunAllSpecs: true,
     env: {
