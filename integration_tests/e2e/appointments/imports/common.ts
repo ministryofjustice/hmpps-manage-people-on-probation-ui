e44f77c10325c19ca2eefe3644f95da3277f5046
Aidan Filby <146082039+AidanFilby@users.noreply.github.com>
2026-04-02 09:07:47 +0100

    MAN - 2392 handling month changes in cypress (#1303)
    * handling month changes in cypress
    
    * MAN-2392 use text field
    
    * fix
    
    * fix
    
    * correct get
    
    * final fix

--- integration_tests/e2e/appointments/imports/common.ts
+++ integration_tests/e2e/appointments/imports/common.ts
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon'
 export const crn = 'X778160'
 export const appointmentId = '6'
 export const uuid = '19a88188-6013-43a7-bb4d-6e338516818f'
-export const date = DateTime.now().plus({ days: 1 }).toISODate()
+export const date = DateTime.now().plus({ days: 2 }).toISODate()
 export const pastDate = DateTime.now().minus({ days: 1 }).toISODate()
 export const startTime = '09:00'
 export const endTime = '10:00'
