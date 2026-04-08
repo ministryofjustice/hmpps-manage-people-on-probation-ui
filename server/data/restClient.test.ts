8586cfebe239a155d216765c38f07fcac240f520
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-04-08 13:52:42 +0100

    persist

--- server/data/restClient.test.ts
+++ server/data/restClient.test.ts
@@ -138,6 +138,7 @@ describe.each(['get', 'post', 'put', 'delete'] as const)('Method: %s', method =>
       nock('http://localhost:8080', { reqheaders: { authorization: 'Bearer token-1' } })
         [method]('/api/test')
         .reply((uri, reqBody, cb) => cb(new Error('This is a test error'), [500, { error: 'This is a test error' }]))
+        .persist()
 
       await expect(restClient[method]<ErrorSummary>({ path: `/test`, handle500: true })).rejects.toThrow(
         'This is a test error',
@@ -185,6 +186,7 @@ describe.each(['get', 'post', 'put', 'delete'] as const)('Method: %s', method =>
       nock('http://localhost:8080', { reqheaders: { authorization: 'Bearer token-1' } })
         [method]('/api/test')
         .reply((uri, reqBody, cb) => cb(new Error('This is a test error'), [500, { error: 'This is a test error' }]))
+        .persist()
 
       await expect(
         restClient[method]<ErrorSummary>({
