5104f7136e2c7bff7246892f8ec9aeaabeb82ad0
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-04-08 12:23:57 +0100

    new rest client for each

--- server/data/restClient.test.ts
+++ server/data/restClient.test.ts
@@ -37,6 +37,18 @@ beforeEach(() => {
 describe.each(['get', 'post', 'put', 'delete'] as const)('Method: %s', method => {
   beforeEach(() => {
     jest.clearAllMocks()
+    restClient = new RestClient(
+      'api-name',
+      {
+        url: 'http://localhost:8080/api',
+        timeout: {
+          response: 1000,
+          deadline: 1000,
+        },
+        agent: new AgentConfig(1000),
+      },
+      'token-1',
+    )
     mockedIsValidHost.mockReturnValue(true)
     mockedIsValidPath.mockReturnValue(true)
   })
