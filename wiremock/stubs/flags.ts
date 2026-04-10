405ee88c76140f5b53080f1cb9c51bd73e537428
Neil Mills <35219002+neil-mills@users.noreply.github.com>
2026-04-10 09:25:31 +0100

    MAN-2067 (#1287)
    * MAN-2067 outcomes page wip
    
    * MAN-2067 fix failing check-ins int test
    
    * MAN-2067 fix activity log integration test
    
    * MAN-2067 fix alerts integration test
    
    * MAN-2066 manage appointment page updates wip
    
    * MAN-2066 refactor appointment stubs
    
    * MAN-2067 add integration tests wip
    
    * MAN-2067 outcome page and tests
    
    * MAN-2067 fix build errors
    
    * MAN-2067 fix dependency cycle build errors
    
    * MAN-2067 fix jest ts error
    
    * Revert "MAN-2067 fix dependency cycle build errors"
    
    This reverts commit fa693ef1c8bfdcaa00a30c86a7da4adeb2417c2b.
    
    * Reapply "MAN-2067 fix dependency cycle build errors"
    
    This reverts commit 0d3a744a5e01e1e7890c89fb5345a808766a3bfc.
    
    * MAN-2067 include add a note page route in outcomes
    
    * MAN-2067 disable feature flag by default in stub
    
    * MAN-2067 fix reschedule int tests
    
    * MAN-2067 update package lock
    
    * MAN-2067 add promise-retry dependency
    
    * MAN-2067 update node build yaml
    
    * Revert "MAN-2067 update node build yaml"
    
    This reverts commit b629d5219ca83653a4d2eb3ca0d269a44e2f7d47.
    
    * MAN-2067 checkout package and node_build from main
    
    * MAN-2067 revert attended complied page and tests
    
    * MAN-2067 revert changes
    
    * MAN-2067 fix rebase issue
    
    * MAN-2067 fix rebase issue with wiremock stub
    
    * MAN-2067 fixes for copilot review comments
    
    * MAN-2067 integration test fix
    
    * MAN-2067 fix failing int test
    
    * MAN-2067 attempted fix for time based integration test failure
    
    * MAN-2067 update mock start and end date/time values
    
    * MAN-2067 updated package lock and fix time error
    
    * MAN-2067 fix build issues
    
    * MAN-2067 fix mock time error

--- wiremock/stubs/flags.ts
+++ wiremock/stubs/flags.ts
@@ -424,17 +424,6 @@ const stubDisableSentencePlanUrl = (): SuperAgentRequest =>
             rules: [],
             rollouts: [],
           },
-          {
-            key: 'enableOGRS4SummaryCardDetail',
-            name: 'enableOGRS4SummaryCardDetail',
-            description: '',
-            enabled: false,
-            type: 'BOOLEAN_FLAG_TYPE',
-            createdAt: '2025-01-13T15:28:37.920581Z',
-            updatedAt: '2025-01-13T17:06:39.269084Z',
-            rules: [],
-            rollouts: [],
-          },
         ],
       },
       headers: {
