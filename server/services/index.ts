a7750a984cb364e556c44f8c8fb14fb324380656
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2026-04-16 18:27:27 +0100

    MAN-2457: Added extra columns to POP search

--- server/services/index.ts
+++ server/services/index.ts
@@ -5,6 +5,30 @@ import FlagService from './flagService'
 import TechnicalUpdatesService from './technicalUpdatesService'
 import config from '../config'
 import ProbationComponentsApiService from './ProbationComponentsService'
+import { convertToTitleCase } from '../utils'
+
+const UNALLOCATED: string = 'Unallocated'
+export const getPdu = (result: any): string => {
+  const activeManager = result?.offenderManagers?.find((item: any) => item?.active)
+  if (!activeManager) {
+    return UNALLOCATED
+  }
+  const pdu = activeManager?.team?.borough?.description
+  return pdu?.includes(UNALLOCATED) ? UNALLOCATED : convertToTitleCase(pdu) || UNALLOCATED
+}
+
+export const getManagedBy = (result: any): string => {
+  const activeManager = result?.offenderManagers?.find((item: any) => item?.active)
+  if (activeManager?.staff?.unallocated || !activeManager) {
+    return UNALLOCATED
+  }
+  const { forenames, surname } = activeManager.staff || {}
+
+  return [forenames, surname]
+    .filter(Boolean)
+    .map(s => convertToTitleCase(s))
+    .join(' ')
+}
 
 export const services = () => {
   const {
@@ -20,7 +44,16 @@ export const services = () => {
   const searchService = new CaseSearchService({
     oauthClient: hmppsAuthClient,
     environment: config.env,
-    extraColumns: [],
+    extraColumns: [
+      {
+        header: 'Managed by',
+        value: result => getManagedBy(result),
+      },
+      {
+        header: 'PDU',
+        value: result => getPdu(result),
+      },
+    ],
   })
 
   const flagService = new FlagService()
