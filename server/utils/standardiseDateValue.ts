2880a15c4c5e7cb8ceb10bcbf98499e8737e3651
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-04-17 09:14:59 +0100

    update ts as well

--- server/utils/standardiseDateValue.ts
+++ server/utils/standardiseDateValue.ts
@@ -6,13 +6,11 @@ export const standardiseDateValue = (dateValue: string | undefined): string | un
   }
   const separators = ['/', '-', '.', ' ', '_', ':']
   const formats: string[] = []
-  for (let i = 0; i < separators.length; i += 1) {
-    const seperator = separators[i]
+  for (const seperator of separators) {
     formats.push(`d${seperator}M${seperator}yyyy`)
     formats.push(`d${seperator}M${seperator}yy`)
   }
-  for (let j = 0; j < formats.length; j += 1) {
-    const format = formats[j]
+  for (const format of formats) {
     const date = DateTime.fromFormat(dateValue, format)
     if (date.isValid) {
       const newDateValue = date.toFormat('d/M/yyyy')
