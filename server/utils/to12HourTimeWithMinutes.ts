43e7a7e939abe266a7ee79f871388efb00161656
Anand Elangovan <Anand.Elangovan1@justice.gov.uk>
2026-04-30 10:46:24 +0100

    [MAN-2540] Address review comments - add to12HourTimeWithMinutes

--- server/utils/to12HourTimeWithMinutes.ts
+++ server/utils/to12HourTimeWithMinutes.ts
@@ -3,6 +3,14 @@
  */
 import { DateTime } from 'luxon'
 
+export const to12HourTimeWithMinutes = (time: string): string => {
+  const [hours, minutes] = time.split(':').map(Number)
+  const period = hours >= 12 ? 'pm' : 'am'
+  const hour12 = hours % 12 || 12
+
+  return `${hour12}:${minutes.toString().padStart(2, '0')}${period}`
+}
+
 export const toIso12HourTimeWithMinutes = (time: string): string => {
   return DateTime.fromISO(time, { zone: 'utc' }) // read as UTC
     .setZone('Europe/London') // convert to UK time
