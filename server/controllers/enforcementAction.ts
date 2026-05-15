5f881dc59131429190accb355b489fde8523c3d7
anandelangovan1 <148061492+anandelangovan1@users.noreply.github.com>
2026-05-15 11:44:02 +0100

    [MAN-2065] My enforcement actions - home page (#1427)
    * [MAN-2065] Display my enforcement actions on the home page

---
+++ server/controllers/enforcementAction.ts
@@ -0,0 +1,40 @@
+import { auditService } from '@ministryofjustice/hmpps-audit-client'
+import { v4 } from 'uuid'
+import getPaginationLinks, { Pagination } from '@ministryofjustice/probation-search-frontend/utils/pagination'
+import { addParameters } from '@ministryofjustice/probation-search-frontend/utils/url'
+import MasApiClient from '../data/masApiClient'
+import { Controller } from '../@types'
+
+const routes = ['getAllEnforcementContacts'] as const
+
+const enforcementContactsController: Controller<typeof routes, void> = {
+  getAllEnforcementContacts: hmppsAuthClient => {
+    return async (req, res) => {
+      const url = encodeURIComponent(req.url)
+      const pageNum: number = req.query.page ? Number.parseInt(req.query.page as string, 10) : 1
+      const token = await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
+      const masClient = new MasApiClient(token)
+
+      await auditService.sendAuditMessage({
+        action: 'VIEW_MAS_ALL_ENFORCEMENT_ACTIONS',
+        who: res.locals.user.username,
+        subjectId: res.locals.user.username,
+        subjectType: 'USER',
+        correlationId: v4(),
+        service: 'hmpps-manage-people-on-probation-ui',
+      })
+
+      const enforcementActions = await masClient.getEnforcementContacts(
+        res.locals.user.username,
+        (pageNum - 1).toString(),
+      )
+
+      return res.render('pages/my-enforcement-actions', {
+        enforcementActions,
+        url,
+      })
+    }
+  },
+}
+
+export default enforcementContactsController
