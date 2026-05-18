27031e944134dd152793d2e5bd02fa52acad4a8b
Neil Mills <neil.mills@digital.justice.gov.uk>
2026-05-18 12:21:47 +0100

    MAN-2609 attempt to fix node build error

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -10,8 +10,6 @@ export default configureAllowedScripts({
     'node_modules/cypress@15.15.0': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.28.0': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Need by playwright for detecting file system changes during test runs
