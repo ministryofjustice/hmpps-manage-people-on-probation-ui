faa60a81a331d864d3076a42adf829100fab9628
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-05-20 14:25:16 +0100

    remove log

--- server/data/masApiClient.ts
+++ server/data/masApiClient.ts
@@ -385,7 +385,6 @@ export default class MasApiClient extends RestClient {
     id: string,
     body: RescheduleAppointmentRequestBody,
   ): Promise<RescheduleAppointmentResponse> {
-    console.log(body)
     return this.put({
       data: body,
       path: `/appointments/${id}/recreate`,
