3c6fb045f5658eae05d653e1d4b5b650de02e7b1
Amardeep Chimber <amardeep.chimber@justice.gov.uk>
2026-05-21 17:55:28 +0100

    MAN-82: following co-pilot review

---
+++ server/testutils/nunjucksTestEnv.ts
@@ -0,0 +1,43 @@
+import nunjucks from 'nunjucks'
+import path from 'path'
+
+import {
+  addressToList,
+  dateWithYear,
+  deliusDeepLinkUrl,
+  fullName,
+  govukTime,
+  handleQuotes,
+  toSentenceCase,
+  yearsSince,
+} from '../utils'
+
+export const createNunjucksTestEnv = () => {
+  const env = nunjucks.configure(
+    [
+      path.join(__dirname, '../views'),
+      'node_modules/govuk-frontend/dist',
+      'node_modules/govuk-frontend/dist/components',
+      'node_modules/@ministryofjustice/frontend',
+      'node_modules/@ministryofjustice/frontend/moj/components',
+      'node_modules/@ministryofjustice/probation-search-frontend/components',
+      'node_modules/@ministryofjustice/hmpps-arns-frontend-components-lib/dist',
+    ],
+    {
+      autoescape: true,
+      noCache: true,
+    },
+  )
+
+  env.addGlobal('addressToList', addressToList)
+  env.addGlobal('deliusDeepLinkUrl', deliusDeepLinkUrl)
+
+  env.addFilter('dateWithYear', dateWithYear)
+  env.addFilter('yearsSince', yearsSince)
+  env.addFilter('toSentenceCase', toSentenceCase)
+  env.addFilter('fullName', fullName)
+  env.addFilter('govukTime', govukTime)
+  env.addFilter('handleQuotes', handleQuotes)
+
+  return env
+}
--- server/views/pages/appointments/manage-appointment.alert-banner.njk.test.ts
+++ server/views/pages/appointments/manage-appointment.alert-banner.njk.test.ts
@@ -1,44 +1,9 @@
 import * as cheerio from 'cheerio'
-import nunjucks from 'nunjucks'
-import path from 'path'
-
-import {
-  addressToList,
-  dateWithYear,
-  deliusDeepLinkUrl,
-  fullName,
-  govukTime,
-  toSentenceCase,
-  yearsSince,
-  handleQuotes,
-} from '../../../utils'
+import { createNunjucksTestEnv } from '../../../testutils/nunjucksTestEnv'
 import { Activity, PersonAppointment } from '../../../data/model/schedule'
 import { PersonSummary } from '../../../data/model/personalDetails'
 
-const env = nunjucks.configure(
-  [
-    path.join(__dirname, '../..'),
-    'node_modules/govuk-frontend/dist',
-    'node_modules/govuk-frontend/dist/components',
-    'node_modules/@ministryofjustice/frontend',
-    'node_modules/@ministryofjustice/frontend/moj/components',
-    'node_modules/@ministryofjustice/probation-search-frontend/components',
-    'node_modules/@ministryofjustice/hmpps-arns-frontend-components-lib/dist',
-  ],
-  {
-    autoescape: true,
-    noCache: true,
-  },
-)
-
-env.addGlobal('addressToList', addressToList)
-env.addGlobal('deliusDeepLinkUrl', deliusDeepLinkUrl)
-env.addFilter('dateWithYear', dateWithYear)
-env.addFilter('yearsSince', yearsSince)
-env.addFilter('toSentenceCase', toSentenceCase)
-env.addFilter('fullName', fullName)
-env.addFilter('govukTime', govukTime)
-env.addFilter('handleQuotes', handleQuotes)
+const env = createNunjucksTestEnv()
 
 const crn = 'X000001'
 const appointmentId = '1'
