ac5472994c563cf2b7ad7d125d7e6fdd74cc6e04
Neil Mills <neil.mills@digital.justice.gov.uk>
2026-05-30 11:11:04 +0100

    MAN-2220 refactor page access rule imports

--- server/middleware/appointment-outcomes/restrictPageAccess.ts
+++ server/middleware/appointment-outcomes/restrictPageAccess.ts
@@ -1,31 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
-import { AppointmentOutcomeType, AppointmentEnforcementAction } from '../../models/Appointments'
 import { getDataValue } from '../../utils'
-
-interface RestrictionRule {
-  url: string
-  required?: RestrictionRuleOption
-  options?: RestrictionRuleOption[]
-}
-
-type Any = '*'
-
-interface RestrictionRuleOption {
-  'outcome.outcomeType'?: AppointmentOutcomeType | Any
-  'outcome.attendedFailedToComply'?: AppointmentEnforcementAction | Any
-  'outcome.acceptableAbsence'?: AppointmentEnforcementAction | Any
-  'outcome.unacceptableAbsence'?: AppointmentEnforcementAction | Any
-  'outcome.failedToAttend'?: AppointmentEnforcementAction | Any
-  'outcome.otherEnforcementAction'?: AppointmentEnforcementAction | Any
-  'outcome.breachNSICreatedBy'?: Any
-  'outcome.letterType'?: Any
-  'outcome.nextAppointment'?: Any
-  'outcome.updateEnforcementAction'?: AppointmentEnforcementAction | Any
-  notes?: Any
-  nextAppointment?: Any
-}
-
-const any: Any = '*'
+import {
+  pageAccessRules,
+  anyValue,
+  type PageAccessRuleItem,
+} from '../../properties/appointment-outcomes/page-access-rules'
 
 export const restrictPageAccess = (req: Request, res: Response, next: NextFunction) => {
   const { url, session, params } = req
@@ -48,295 +27,40 @@ export const restrictPageAccess = (req: Request, res: Response, next: NextFuncti
     return res.redirect(originUrl)
   }
 
-  const allOptions: RestrictionRuleOption[] = [
-    { 'outcome.outcomeType': 'ATTENDED_COMPLIED' },
-    { 'outcome.outcomeType': 'ATTENDED_SENT_HOME_SERVICE_ISSUES' },
-    {
-      'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-      'outcome.attendedFailedToComply': 'SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-      'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED',
-      'outcome.breachNSICreatedBy': any,
-    },
-    {
-      'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-      'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-      'outcome.attendedFailedToComply': 'SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-      'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED',
-      'outcome.breachNSICreatedBy': any,
-    },
-    {
-      'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-      'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-      'outcome.attendedFailedToComply': 'REFER_TO_OFFENDER_MANAGER',
-    },
-    { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY', 'outcome.attendedFailedToComply': 'NO_FURTHER_ACTION' },
-    {
-      'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-      'outcome.attendedFailedToComply': 'DIFFERENT_ACTION',
-      'outcome.otherEnforcementAction': any,
-    },
-    { 'outcome.outcomeType': 'ACCEPTABLE_ABSENCE', 'outcome.acceptableAbsence': any },
-    {
-      'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE',
-      'outcome.unacceptableAbsence': 'SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE',
-      'outcome.unacceptableAbsence': 'BREACH_RECALL_INITIATED',
-      'outcome.breachNSICreatedBy': any,
-    },
-    {
-      'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE',
-      'outcome.unacceptableAbsence': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.unacceptableAbsence': 'REFER_TO_OFFENDER_MANAGER' },
-    { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.unacceptableAbsence': 'NO_FURTHER_ACTION' },
-    {
-      'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE',
-      'outcome.unacceptableAbsence': 'DIFFERENT_ACTION',
-      'outcome.otherEnforcementAction': any,
-    },
-    { 'outcome.outcomeType': 'FAILED_TO_ATTEND', 'outcome.failedToAttend': 'SEND_LETTER', 'outcome.letterType': any },
-    {
-      'outcome.outcomeType': 'FAILED_TO_ATTEND',
-      'outcome.failedToAttend': 'DECISION_PENDING_RESPONSE_FROM_PERSON_ON_PROBATION',
-    },
-    {
-      'outcome.outcomeType': 'FAILED_TO_ATTEND',
-      'outcome.failedToAttend': 'DIFFERENT_ACTION',
-      'outcome.otherEnforcementAction': any,
-    },
-    { 'outcome.outcomeType': 'FAILED_TO_ATTEND', 'outcome.failedToAttend': 'REFER_TO_OFFENDER_MANAGER' },
-    {
-      'outcome.outcomeType': any,
-      'outcome.updateEnforcementAction': 'BREACH_RECALL_INITIATED',
-      'outcome.breachNSICreatedBy': any,
-    },
-    {
-      'outcome.outcomeType': any,
-      'outcome.updateEnforcementAction': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': any,
-      'outcome.updateEnforcementAction': 'SEND_ANOTHER_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': any,
-      'outcome.updateEnforcementAction': 'SEND_LETTER',
-      'outcome.letterType': any,
-    },
-    {
-      'outcome.outcomeType': any,
-      'outcome.updateEnforcementAction': 'DIFFERENT_ACTION',
-      'outcome.otherEnforcementAction': any,
-    },
-    {
-      'outcome.outcomeType': any,
-      'outcome.otherEnforcementAction': any,
-    },
-    {
-      'outcome.outcomeType': any,
-      'outcome.updateEnforcementAction': any,
-    },
-  ]
-
-  const rules: RestrictionRule[] = [
-    {
-      url: 'attended-failed-to-comply',
-      options: [
-        { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY' },
-        { 'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR' },
-      ],
-    },
-    {
-      url: 'acceptable-absence',
-      required: { 'outcome.outcomeType': 'ACCEPTABLE_ABSENCE' },
-    },
-    {
-      url: 'unacceptable-absence',
-      required: { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE' },
-    },
-    {
-      url: 'failed-to-attend',
-      required: { 'outcome.outcomeType': 'FAILED_TO_ATTEND' },
-    },
-    {
-      url: 'enforcement-action',
-      options: [
-        { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY', 'outcome.attendedFailedToComply': 'DIFFERENT_ACTION' },
-        { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY', 'outcome.attendedFailedToComply': 'NO_FURTHER_ACTION' },
-        { 'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR', 'outcome.attendedFailedToComply': 'DIFFERENT_ACTION' },
-        {
-          'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-          'outcome.attendedFailedToComply': 'NO_FURTHER_ACTION',
-        },
-        { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.unacceptableAbsence': 'DIFFERENT_ACTION' },
-        { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.unacceptableAbsence': 'NO_FURTHER_ACTION' },
-        { 'outcome.outcomeType': 'FAILED_TO_ATTEND', 'outcome.failedToAttend': 'DIFFERENT_ACTION' },
-        { 'outcome.outcomeType': any, 'outcome.otherEnforcementAction': any },
-        {
-          'outcome.outcomeType': any,
-          'outcome.updateEnforcementAction': 'DIFFERENT_ACTION',
-        },
-      ],
-    },
-    {
-      url: 'initiate-breach-or-recall',
-      options: [
-        {
-          'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-          'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED',
-        },
-        {
-          'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-          'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-          'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED',
-        },
-        {
-          'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-          'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-          'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED',
-        },
-        {
-          'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-          'outcome.attendedFailedToComply': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-        },
-        { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.unacceptableAbsence': 'BREACH_RECALL_INITIATED' },
-        {
-          'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE',
-          'outcome.unacceptableAbsence': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': any,
-          'outcome.updateEnforcementAction': 'BREACH_RECALL_INITIATED',
-        },
-        {
-          'outcome.outcomeType': any,
-          'outcome.updateEnforcementAction': 'BREACH_RECALL_INITIATED_AND_SEND_LETTER',
-        },
-      ],
-    },
-    {
-      url: 'send-letter',
-      options: [
-        {
-          'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY',
-          'outcome.attendedFailedToComply': 'SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': any,
-          'outcome.updateEnforcementAction': 'SEND_ANOTHER_LETTER',
-        },
-        {
-          'outcome.outcomeType': any,
-          'outcome.updateEnforcementAction': 'SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': 'ATTENDED_SENT_HOME_BEHAVIOUR',
-          'outcome.attendedFailedToComply': 'SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE',
-          'outcome.unacceptableAbsence': 'SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': 'FAILED_TO_ATTEND',
-          'outcome.failedToAttend': 'SEND_LETTER',
-        },
-        {
-          'outcome.outcomeType': 'FAILED_TO_ATTEND',
-          'outcome.failedToAttend': 'SEND_LETTER',
-        },
-      ],
-    },
-    {
-      url: 'add-note',
-      options: allOptions,
-    },
-    {
-      url: 'update-enforcement-action',
-      options: [
-        { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY', 'outcome.attendedFailedToComply': any },
-        { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY', 'outcome.letterType': any },
-        { 'outcome.outcomeType': 'ATTENDED_FAILED_TO_COMPLY', 'outcome.otherEnforcementAction': any },
-        { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.unacceptableAbsence': any },
-        { 'outcome.outcomeType': 'UNACCEPTABLE_ABSENCE', 'outcome.letterType': any },
-        { 'outcome.outcomeType': 'FAILED_TO_ATTEND', 'outcome.failedToAttend': any },
-        { 'outcome.outcomeType': 'FAILED_TO_ATTEND', 'outcome.letterType': any },
-      ],
-    },
-    {
-      url: 'next-appointment',
-      required: { notes: any },
-      options: allOptions,
-    },
-    {
-      url: 'check-your-answers',
-      options: [...allOptions, { 'outcome.outcomeType': any }],
-    },
-  ]
-
   let restricted = false
 
-  const checkRequiredValue = (key: keyof RestrictionRuleOption, value: string): boolean => {
+  const checkRequiredValue = (key: keyof PageAccessRuleItem, value: string): boolean => {
     const keys = key.split('.')
     const sessionValue = getDataValue(data, [...path, ...keys])
     let isInvalid = sessionValue === undefined
-    if (value !== any) {
+    if (value !== anyValue) {
       isInvalid = sessionValue !== value
     }
     return isInvalid
   }
 
-  const checkOptions = (options: RestrictionRuleOption[]): boolean => {
-    for (const option of options) {
-      let isInvalidOption = false
-      for (const [key, value] of Object.entries(option) as [keyof RestrictionRuleOption, string][]) {
-        isInvalidOption = checkRequiredValue(key, value)
+  const checkPageAccessRules = (rules: PageAccessRuleItem[]): boolean => {
+    for (const rule of rules) {
+      let isInvalidRule = false
+      for (const [key, value] of Object.entries(rule) as [keyof PageAccessRuleItem, string][]) {
+        isInvalidRule = checkRequiredValue(key, value)
       }
-      if (!isInvalidOption) {
+      if (!isInvalidRule) {
         return false
-        break
       }
     }
     return true
   }
 
-  for (const rule of rules) {
+  for (const rule of pageAccessRules) {
     if (url.includes(`outcome/${rule.url}`)) {
       if (rule?.required) {
-        for (const [key, value] of Object.entries(rule.required) as [keyof RestrictionRuleOption, string][]) {
+        for (const [key, value] of Object.entries(rule.required) as [keyof PageAccessRuleItem, string][]) {
           restricted = checkRequiredValue(key, value)
         }
       }
-      if (rule?.options && !restricted) {
-        restricted = checkOptions(rule.options)
+      if (rule?.oneRequired && !restricted) {
+        restricted = checkPageAccessRules(rule.oneRequired)
       }
     }
   }
