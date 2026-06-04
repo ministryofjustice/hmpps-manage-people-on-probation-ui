b095c96143db015cfb4b4e8ce4c0c1370c0e773d
Luca Sanz <luca.sanz@justice.gov.uk>
2026-06-04 08:40:45 +0100

    tests branch protection

--- server/data/masApiClient.ts
+++ server/data/masApiClient.ts
@@ -86,7 +86,8 @@ export default class MasApiClient extends RestClient {
 
   async getOverview(crn: string, sentenceNumber = '1'): Promise<Overview | null> {
     const queryParam = `?sentenceNumber=${sentenceNumber}`
-    return this.get({ path: `/overview/${crn}${queryParam}`, handle404: false })
+    const overview: Overview | null = (await this.get({ path: `/overview/${crn}${queryParam}`, handle404: false })) as Overview | null
+    return overview
   }
 
   async getOverdueOutcomes(crn: string): Promise<ContactResponse | null> {
