51fbb93a4b1a3401f6d1b3a18cbb824b94cf71e5
Anand Elangovan <Anand.Elangovan1@justice.gov.uk>
2026-06-09 11:58:22 +0100

    [MAN-2782] wip - redirect to add note when selecting attended-sent home

--- server/properties/appointment-outcomes/outcome-redirect-map.ts
+++ server/properties/appointment-outcomes/outcome-redirect-map.ts
@@ -7,7 +7,7 @@ export type OutcomeRedirectMap = {
 export const outcomeRedirectMap = (baseOutcomeUrl: string): OutcomeRedirectMap => ({
   ATTENDED_COMPLIED: `${baseOutcomeUrl}/add-note`,
   ATTENDED_FAILED_TO_COMPLY: `${baseOutcomeUrl}/attended-failed-to-comply`,
-  ATTENDED_SENT_HOME_BEHAVIOUR: `${baseOutcomeUrl}/attended-failed-to-comply`,
+  ATTENDED_SENT_HOME_BEHAVIOUR: `${baseOutcomeUrl}/add-note`,
   ATTENDED_SENT_HOME_SERVICE_ISSUES: `${baseOutcomeUrl}/add-note`,
   ACCEPTABLE_ABSENCE: `${baseOutcomeUrl}/acceptable-absence`,
   UNACCEPTABLE_ABSENCE: `${baseOutcomeUrl}/unacceptable-absence`,
