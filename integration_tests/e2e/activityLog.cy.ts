8405072d07aee187a35eea4ee748cc4bffc3cc51
Gerald Isabelle <gerald.isabelle@justice.gov.uk>
2026-06-12 17:00:41 +0100

    MAN-2588: contact log, Manage on delius for Alcohol Consumption, Critical Communications and Unplanned contact from person on probation

--- integration_tests/e2e/activityLog.cy.ts
+++ integration_tests/e2e/activityLog.cy.ts
@@ -602,4 +602,23 @@ context('Contacts', () => {
     page.getActivity(1).should('contain.text', 'AP PA - attitudes, thinking & behaviours')
     page.getActivity(2).should('contain.text', 'Pre-intervention session 1')
   })
+
+  it('should show Manage on NDelius link for enforcement contact types when flag is enabled', () => {
+    cy.task('stubActivityLogWithAlcoholConsumption')
+    cy.visit('/case/X000001/activity-log')
+    const page = Page.verifyOnPage(ActivityLogPage)
+    page.getActivity(0).should('contain.text', 'Alcohol consumption')
+    page.getElementByDataQA('manage-on-delius-link').should('be.visible')
+    page.getElementByDataQA('manage-on-delius-link').should('contain.text', 'Manage on NDelius')
+  })
+
+  it('should show standard Manage link for enforcement contact types when flag is disabled', () => {
+    cy.task('stubDisableEnforcementContacts')
+    cy.task('stubActivityLogWithAlcoholConsumption')
+    cy.visit('/case/X000001/activity-log')
+    const page = Page.verifyOnPage(ActivityLogPage)
+    page.getActivity(0).should('contain.text', 'Alcohol consumption')
+    page.getElementByDataQA('manage-on-delius-link').should('not.exist')
+    page.getActivityViewLink(0).should('contain.text', 'Manage')
+  })
 })
