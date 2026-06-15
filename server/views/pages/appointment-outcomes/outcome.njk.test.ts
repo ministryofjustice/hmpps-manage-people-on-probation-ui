193590bff772637317779a056de190139416e48d
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-06-15 09:24:04 +0100

    comment progress

--- server/views/pages/appointment-outcomes/outcome.njk.test.ts
+++ server/views/pages/appointment-outcomes/outcome.njk.test.ts
@@ -104,6 +104,118 @@ const render = (model = {} as Partial<TestModel>) => {
   return cheerio.load(env.render('pages/appointment-outcomes/outcome.njk', input))
 }
 
+// const getExpectedOptions = ({ inOffice = true, dateInPast = true }): ExpectedOption<RedirectPages>[] => {
+//   const options: ExpectedOption<RedirectPages>[] = []
+//   if (dateInPast) {
+//     options.push(
+//       {
+//         value: 'ATTENDED_COMPLIED',
+//         text: 'Attended - complied',
+//         redirectPageTitle: 'Add a note',
+//         RedirectPage: AddNotePage,
+//       },
+//       {
+//         value: 'ATTENDED_FAILED_TO_COMPLY',
+//         text: 'Attended - failed to comply',
+//         hint: 'For example, they did not follow instructions.',
+//         redirectPageTitle: 'Enforcement action for Alton’s failure to comply',
+//         RedirectPage: AttendedFailedToComplyPage,
+//       },
+//     )
+//     if (inOffice) {
+//       options.push(
+//         {
+//           value: 'ATTENDED_SENT_HOME_BEHAVIOUR',
+//           text: 'Attended - sent home (behaviour)',
+//           hint: 'For example, their behaviour was disruptive',
+//           redirectPageTitle: 'Enforcement action for Alton’s failure to comply',
+//           RedirectPage: AttendedFailedToComplyPage,
+//         },
+//         {
+//           value: 'ATTENDED_SENT_HOME_SERVICE_ISSUES',
+//           text: 'Attended - sent home (service issues)',
+//           hint: 'For example, the building was unexpectedly closed.',
+//           redirectPageTitle: 'Add a note',
+//           RedirectPage: AddNotePage,
+//         },
+//       )
+//     }
+//   }
+//   options.push({
+//     value: 'ACCEPTABLE_ABSENCE',
+//     text: 'Acceptable absence',
+//     hint: 'They provided an acceptable reason or evidence.',
+//     redirectPageTitle: 'Why was Alton’s absence acceptable?',
+//     RedirectPage: AcceptableAbsencePage,
+//   })
+//   if (dateInPast) {
+//     options.push(
+//       {
+//         value: 'UNACCEPTABLE_ABSENCE',
+//         text: 'Unacceptable absence',
+//         hint: 'They did not provide suitable evidence.',
+//         redirectPageTitle: 'Enforcement action for Alton’s unacceptable absence',
+//         RedirectPage: UnacceptableAbsencePage,
+//       },
+//       {
+//         value: 'FAILED_TO_ATTEND',
+//         text: 'Failed to attend',
+//         hint: 'You may still need to request and review evidence.',
+//         redirectPageTitle: 'Enforcement action for Alton’s absence',
+//         RedirectPage: FailedToAttendPage,
+//       },
+//     )
+//   }
+//   if (!dateInPast) {
+//     options.push({
+//       value: 'WILL_BE_RESCHEDULED',
+//       text: 'The appointment will be rescheduled',
+//       redirectPageTitle: 'Reschedule an appointment',
+//       RedirectPage: RescheduleAppointmentPage,
+//     })
+//   }
+//   return options
+// }
+
+// export const checkOptions = <TPage extends Page>(options: ExpectedOption<TPage>[], radioGroupIndex = 0): void => {
+//   options.forEach(({ value, text, hint }, index) => {
+//     cy.get('[data-module="govuk-radios"]')
+//       .eq(radioGroupIndex)
+//       .find('.govuk-radios__item')
+//       .eq(index)
+//       .find('label')
+//       .should('contain.text', text)
+//     if (hint) {
+//       cy.get('[data-module="govuk-radios"]')
+//         .eq(radioGroupIndex)
+//         .find('.govuk-radios__item')
+//         .eq(index)
+//         .find('.govuk-hint')
+//         .should('contain.text', hint)
+//     }
+//     cy.get('[data-module="govuk-radios"]')
+//       .eq(radioGroupIndex)
+//       .find(`.govuk-radios__input[value=${value}]`)
+//       .should('exist')
+//   })
+// }
+
+// const checkValidationErrors = ({
+//   journey = 'MANAGE',
+//   inOffice = true,
+//   dateInPast = true,
+// }: { journey?: Journey; inOffice?: boolean; dateInPast?: boolean } = {}): void => {
+//   const msg = dateInPast ? 'Select an outcome for this appointment' : 'Select why they will not attend this appointment'
+//   loadPage({ journey, inOffice, dateInPast })
+//   outcomePage = new OutcomePage()
+//   outcomePage.getSubmitBtn().click()
+//   outcomePage.checkErrorSummaryBox([msg])
+//   getUuid(2).then(pageUuid => {
+//     const id = journey === 'MANAGE' ? appointmentId : pageUuid
+//     cy.get(`#appointments-${crn}-${id}-outcome-outcomeType-error`).should('contain.text', msg)
+//   })
+// }
+
 describe('Manage appointment journey', () => {
   describe('Appointment is in the past and in office', () => {
     it('should render the page', () => {
