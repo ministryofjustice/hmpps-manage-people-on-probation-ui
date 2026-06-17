8a03f7a6eb077abfa6240dccb9bb07a13578310e
anandelangovan1 <148061492+anandelangovan1@users.noreply.github.com>
2026-06-17 11:24:46 +0100

    [MTRSB-96] Remove enableSentencePlanUrl featureflag (#1603)

--- server/data/model/featureFlags.ts
+++ server/data/model/featureFlags.ts
@@ -7,7 +7,6 @@ export class FeatureFlags {
   enableDeliusClient?: boolean = undefined
   enableMAN2344?: boolean = undefined
   enableNonCompliance?: boolean = undefined
-  enableSentencePlanUrl?: boolean = undefined
   enableDeepLinks?: boolean = undefined
   enableOutcomesV1?: boolean = undefined
   enableESupervisionCheckins?: boolean = undefined
