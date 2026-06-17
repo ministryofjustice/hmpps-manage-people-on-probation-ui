bce246cc2069846fb39ebdb4c3007f5d68a6f581
Neil Mills <neil.mills@digital.justice.gov.uk>
2026-06-17 09:29:36 +0100

    MAN-2903 fix failing test

--- integration_tests/e2e/appointmentOutcomes/acceptable-absence.cy.ts
+++ integration_tests/e2e/appointmentOutcomes/acceptable-absence.cy.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon'
 import { checkPopHeader } from '../appointments/imports'
 import { crn, appointmentId } from '../appointments/imports/common'
 import ManageAppointmentPage from '../../pages/appointments/manage-appointment.page'
@@ -25,6 +26,7 @@ import {
 import AcceptableAbsencePage from '../../pages/appointmentOutcomes/acceptable-absence.page'
 import RescheduleCheckYourAnswerPage from '../../pages/appointments/reschedule-check-your-answer.page'
 import { SentenceType } from '../../../server/data/model/sentenceDetails'
+import { dateWithDayAndWithYear } from '../../../server/utils'
 
 let manageAppointmentPage: ManageAppointmentPage
 let outcomePage: OutcomePage
@@ -137,11 +139,14 @@ const checkPage = ({ journey = 'MANAGE' }: { journey?: Journey } = {}) => {
     checkPopHeader({ name: 'Alton Berge', appointments: true, headerCrn: crn })
     cy.get('legend').should('contain.text', 'Why was Alton’s absence acceptable?')
     let expectedAppointment = '3 way meeting (NS) with Terry Jones on Wednesday 21 February 2024'
+    const now = DateTime.now()
+    const yesterday = now.minus({ days: 1 })
+    const expectedDate = dateWithDayAndWithYear(yesterday.toISODate())
     if (journey === 'ARRANGE') {
-      expectedAppointment = 'Planned office visit (NS) with Deborah Fern on Monday 15 June 2026'
+      expectedAppointment = `Planned office visit (NS) with Deborah Fern on ${expectedDate}`
     }
     if (journey === 'RESCHEDULE') {
-      expectedAppointment = '3 way meeting (NS) with Terry Jones on Monday 15 June 2026'
+      expectedAppointment = `3 way meeting (NS) with Terry Jones on ${expectedDate}`
     }
     cy.get('.govuk-hint').should('contain.text', `Appointment: ${expectedAppointment}.`)
     const options = getExpectedOptions()
