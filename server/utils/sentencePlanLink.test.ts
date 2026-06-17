b689bfe33b743895c2ac0471ff99415a4d9983b7
anandelangovan1 <148061492+anandelangovan1@users.noreply.github.com>
2026-06-17 11:24:46 +0100

    [MTRSB-96] Remove enableSentencePlanUrl featureflag (#1603)

--- server/utils/sentencePlanLink.test.ts
+++ server/utils/sentencePlanLink.test.ts
@@ -2,10 +2,7 @@ import config from '../config'
 import { sentencePlanLink } from './sentencePlanLink'
 
 describe('utils/sentencePlanLink', () => {
-  it('should return the sentence plan link from config', () => {
-    expect(sentencePlanLink()).toEqual(config.sentencePlan.link)
-  })
   it('should return the sentence plan link v2 from config', () => {
-    expect(sentencePlanLink(true)).toEqual(config.sentencePlan.linkV2)
+    expect(sentencePlanLink()).toEqual(config.sentencePlan.linkV2)
   })
 })
