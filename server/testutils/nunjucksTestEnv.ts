dc95b329814169b372a537d2b0e959d225e5d9e4
Luca2471 <57104182+Luca2471@users.noreply.github.com>
2026-06-18 08:09:41 +0100

    [MAN-2838]: connecting to the shared component lib (#1579)

--- server/testutils/nunjucksTestEnv.ts
+++ server/testutils/nunjucksTestEnv.ts
@@ -23,6 +23,7 @@ export const createNunjucksTestEnv = () => {
       'node_modules/@ministryofjustice/frontend/moj/components',
       'node_modules/@ministryofjustice/probation-search-frontend/components',
       'node_modules/@ministryofjustice/hmpps-arns-frontend-components-lib/dist',
+      'node_modules/@ministryofjustice/hmpps-mpop-frontend-components-lib/dist',
     ],
     {
       autoescape: true,
