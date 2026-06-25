621dd4eb858c64acc3eb75168da20c68479a1fe8
Aidan Filby <146082039+AidanFilby@users.noreply.github.com>
2026-06-25 15:38:19 +0100

    resolve dependabot alerts (#1659)
    * resolve dependabot alerts
    
    * resolve dependabot alerts
    
    * audit fix
    
    * audit fix

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -20,6 +20,5 @@ export default configureAllowedScripts({
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
     // required for open telemetry for app insights
     'node_modules/@grpc/proto-loader/node_modules/protobufjs@7.6.4': 'ALLOW',
-    'node_modules/protobufjs@8.0.1': 'ALLOW',
   },
 })
