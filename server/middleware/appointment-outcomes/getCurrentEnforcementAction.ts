f6da0fb97612238c11b35e12fd366e35dd6e6a4d
Anand Elangovan <Anand.Elangovan1@justice.gov.uk>
2026-06-30 14:49:46 +0100

    [MAN-3053] Enforcement action to be displayed in sentence case

--- server/middleware/appointment-outcomes/getCurrentEnforcementAction.ts
+++ server/middleware/appointment-outcomes/getCurrentEnforcementAction.ts
@@ -17,10 +17,6 @@ export const getCurrentEnforcementAction: Route<void> = (_req, res, next): void 
   let evidenceWarning: string = null
   const { enforcementAction = null, appointment } = res.locals.personAppointment
 
-  console.log('********* enforcementAction ***************')
-  console.dir(enforcementAction, { depth: null })
-  console.log('************************')
-
   if (enforcementAction) {
     const { description = '', responseByDate = null, code: actionCode } = enforcementAction
     let action: AppointmentEnforcementAction = null
@@ -57,12 +53,6 @@ export const getCurrentEnforcementAction: Route<void> = (_req, res, next): void 
       evidenceDueDate,
       evidenceWarning,
     }
-
-    console.log('********* enforcementActionMap ***************')
-    console.dir(enforcementActionMap, { depth: null })
-    console.log('*********currentEnforcementAction***************')
-    console.dir(currentEnforcementAction, { depth: null })
-    console.log('************************')
   }
   res.locals.appointmentOutcome.currentEnforcementAction = currentEnforcementAction
   return next()
