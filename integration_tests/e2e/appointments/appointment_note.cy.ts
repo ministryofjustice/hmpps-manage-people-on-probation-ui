ba6eb1156b43998fe5b27cdfe0c684604599ca58
Akshay-MOJ <Akshay.Ramchurn@justice.gov.uk>
2026-07-01 11:15:17 +0100

    fix int test

--- integration_tests/e2e/appointments/appointment_note.cy.ts
+++ integration_tests/e2e/appointments/appointment_note.cy.ts
@@ -15,11 +15,12 @@ describe('View appointment note page', () => {
     page.getRowData('appointmentDetails', 'time', 'Value').should('contain.text', '9:15am to 9:30am')
     page.getRowData('appointmentDetails', 'rar', 'Value').should('contain.text', 'Stepping Stones')
     page.getRowData('appointmentDetails', 'rarActivity', 'Value').should('contain.text', 'Not provided')
-    page.getRowData('appointmentDetails', 'noteAddedBy', 'Value').should('contain.text', 'Tom Brady')
-    page.getRowData('appointmentDetails', 'dateAdded', 'Value').should('contain.text', '29 October 2024')
-    page.getRowData('appointmentDetails', 'note', 'Value').should('contain.text', 'Email sent to Stuart')
-    page.getRowData('appointmentDetails', 'sensitive', 'Label').should('contain.text', 'No')
+    page.getElement('.app-note__author').should('contain.text', 'Tom Brady')
+    page.getElement('.app-note__author').should('contain.text', '29 October 2024')
+    page.getElementData('notesValue').should('contain.text', 'Email sent to Stuart')
+    page.getElement('[data-qa="sensitiveLabel"]').should('contain.text', 'No')  
   })
+    
   it('Back link goes to contacts by default', () => {
     cy.visit('/case/X000001/appointments/appointment/11/manage/note/1')
     const page = new AppointmentPage()
