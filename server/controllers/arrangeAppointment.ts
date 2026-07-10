94d09c5e900ed8beac7e975bfc50a9886221fde4
Amardeep Chimber <amardeep.chimber@justice.gov.uk>
2026-07-10 13:31:19 +0100

    Revert "Potential fix for pull request finding"
    This reverts commit 4f28c31d7a59acc50cbd6a099c68fc77b88645d9.

--- server/controllers/arrangeAppointment.ts
+++ server/controllers/arrangeAppointment.ts
@@ -242,18 +242,9 @@ const arrangeAppointmentController: Controller<typeof routes, void | AppResponse
       const username = body?.appointments?.[crn]?.[id]?.temp?.username
       const staff = getDataValue<User[]>(data, ['staff', res.locals.user.username])
       const staffMember = staff?.find(person => person.username === username)
-      if (res.locals.flags.enableSessionCacheLogging)
-        logger.info(
-          {
-            event: 'sessionCacheTrace',
-            source: 'postWhoWillAttend',
-            uuid: id,
-            loggedInUser: res.locals.user.username,
-            selectedUsername: username,
-            staffMemberFound: Boolean(staffMember),
-          },
-          '[postWhoWillAttend] selection details',
-        )
+      logger.info(
+        `[postWhoWillAttend] uuid='${id}' loggedInUser='${res.locals.user.username}' selectedUsername='${username}' staffMemberFound=${Boolean(staffMember)}`,
+      )
       if (providerCode) {
         const postWhoWillAttendContext = { uuid: id, username, enabled: res.locals.flags.enableSessionCacheLogging }
         logSessionCacheChange(
