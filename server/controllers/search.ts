f79ccae2536308d05a5800f5079dab898461b2e0
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-08-13 17:14:35 +0100

    MAN-3265

--- server/controllers/search.ts
+++ server/controllers/search.ts
@@ -1,10 +1,9 @@
-import { DateTime } from 'luxon'
 import { Readable } from 'stream'
 import { Controller } from '../@types'
-import { ProbationSearchRequest, ProbationSearchResponse } from '../data/model/search'
 import sendAuditMessage, { SubjectType } from '../middleware/sendAuditMessage'
 import { HmppsAuthClient } from '../data'
 import PrisonApiClient from '../data/prisonApiClient'
+import { mapResults } from '../utils/mapResults'
 
 const routes = ['getSearch', 'getPhoto'] as const
 
@@ -43,40 +42,6 @@ const searchController: Controller<typeof routes, void> = {
       }
     }
   },
-}
-
-function mapResults(response: ProbationSearchResponse, request: ProbationSearchRequest) {
-  const returnedProviders = response.probationAreaAggregations.map(p => ({
-    value: `${p.code}-${p.description}`,
-    text: `${p.description} (${p.count})`,
-    checked: request.providersFilter.includes(`${p.code}-${p.description}`),
-  }))
-  const selectedProviders = request.providersFilter
-    .filter(p => !returnedProviders.find(r => r.value === p))
-    .map(p => ({
-      value: p,
-      text: `${p.substring(4)} (0)`,
-      checked: true,
-    }))
-  return {
-    content: response.content.map(result => {
-      const activeManager = result.offenderManagers?.filter(manager => manager.active).shift()
-      return {
-        ...result,
-        formattedDateOfBirth: result.dateOfBirth ? DateTime.fromISO(result.dateOfBirth).toFormat('dd/MM/yyyy') : '',
-        imageUrl: result.otherIds.nomsNumber
-          ? `/search/prisoner-image/${result.otherIds.nomsNumber}`
-          : '/assets/images/NoPhoto@2x.png',
-        officer: `${activeManager?.staff?.surname}, ${activeManager?.staff?.forenames}`,
-        provider: activeManager?.probationArea?.description,
-      }
-    }),
-    query: request.query,
-    providers: [...selectedProviders, ...returnedProviders].sort(
-      (a, b) => +b.checked - +a.checked || a.text?.localeCompare(b.text),
-    ),
-    matchAllTerms: request.matchAllTerms,
-  }
 }
 
 export default searchController
