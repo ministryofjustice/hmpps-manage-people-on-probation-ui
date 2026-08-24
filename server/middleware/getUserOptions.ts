be127b5224f572530cf6243b314a949a0042a3c9
Neil Mills <neil.mills@digital.justice.gov.uk>
2026-08-24 12:44:40 +0100

    MAN-3255 add fix behind feature flag

--- server/middleware/getUserOptions.ts
+++ server/middleware/getUserOptions.ts
@@ -55,13 +55,26 @@ export const getUserOptions = (hmppsAuthClient: HmppsAuthClient): Route<Promise<
       team => team.description.toLowerCase() === defaultUserDetails?.team?.toLowerCase(),
     )?.code
 
-    let selectedTeam = teamCodeQuery ?? teamCodeQuery
+    let selectedTeam: string
+    if (res.locals.flags.enableMAN3255) {
+      selectedTeam = teamCodeQuery
+    } else {
+      selectedTeam = teamCodeQuery ?? teamCodeQuery
+    }
     if (selectedTeam === probationPractitioner.team.code) {
-      selectedTeam = defaultTeam ?? teams[0].code
+      if (res.locals.flags.enableMAN3255) {
+        selectedTeam = teams[0].code
+      } else {
+        selectedTeam = defaultTeam ?? teams[0].code
+      }
     }
     if (!providerCodeQuery) {
       if (selectedProvider === defaultProvider) {
-        selectedTeam = defaultTeam ?? teams[0].code
+        if (res.locals.flags.enableMAN3255) {
+          selectedTeam = teams[0].code
+        } else {
+          selectedTeam = defaultTeam ?? teams[0].code
+        }
       } else {
         selectedTeam = teamCodeSession ?? teams[0].code
       }
