d25486be1802de73cba1efe6eca982a7a6cc5f0b
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-09-01 11:36:12 +0100

    MAN-3349-remove-validation

--- server/properties/validation/activityLog.ts
+++ server/properties/validation/activityLog.ts
@@ -13,10 +13,6 @@ export const activityLogValidation = (dateToIsEmpty = true, dateFromIsEmpty = tr
         validator: isValidDate,
         msg: 'Enter a date in the correct format, for example 17/5/2024',
       },
-      {
-        validator: isNotLaterThanToday,
-        msg: 'The date from must be today or in the past',
-      },
     ],
   },
   dateTo: {
@@ -29,10 +25,6 @@ export const activityLogValidation = (dateToIsEmpty = true, dateFromIsEmpty = tr
       {
         validator: isValidDate,
         msg: 'Enter a date in the correct format, for example 17/5/2024',
-      },
-      {
-        validator: isNotLaterThanToday,
-        msg: 'The date to must be today or in the past',
       },
       {
         validator: isNotLaterThan,
