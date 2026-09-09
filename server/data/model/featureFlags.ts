8c1cd9fc0e04c5d46294b62cdc5ed155545fe737
Neil Mills <35219002+neil-mills@users.noreply.github.com>
2026-09-09 17:06:41 +0100

    MRTSB-142 remove EMDIOverviewShowGPSData flag (#1974)

--- server/data/model/featureFlags.ts
+++ server/data/model/featureFlags.ts
@@ -13,7 +13,6 @@ export class FeatureFlags {
   enableESupervisionCheckins?: boolean = undefined
   enableHomePageOutcomesWithFilter?: boolean = undefined
   enableMyEnforcementActionsOverview?: boolean = undefined
-  enableEMDIOverviewShowGPSData?: boolean = undefined
   enableEnforcementContacts?: boolean = undefined
   enableUserEditableActions?: boolean = undefined
   enableBreachOrRecallAndSendLetterAction?: boolean = undefined
