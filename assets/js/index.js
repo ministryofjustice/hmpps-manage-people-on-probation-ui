b48be01d5173c6ca81fd467e08396d25f2ac56f6
Aidan Filby <Aidan.Filby@tpximpact.com>
2026-09-15 16:18:54 +0100

    MAN-2988-fix-js

--- assets/js/index.js
+++ assets/js/index.js
@@ -85,7 +85,10 @@ const attendanceSelectors = () => {
   const providerSelect = document.querySelector('[data-qa="providerCode"]')
   const teamSelect = document.querySelector('[data-qa="teamCode"]')
   const userSelect = document.querySelector('[data-qa="username"]')
-  document.getElementById('update-btn').classList.add('no-js')
+  const updateButton = document.getElementById('update-btn')
+  if (updateButton) {
+    updateButton.classList.add('no-js')
+  }
   const submitButton = document.getElementById('submit-btn')
   const announcer = document.getElementById('update-announcer')
   let providerCode = ''
